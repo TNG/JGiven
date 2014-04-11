43a58c364c4d5b0c21d04de29f0aa635736a40fc
Jan Schäfer <jan.schaefer@tngtech.com>
2014-04-11 16:04:49 +0200

    Added SzenarioTest for tests written in german

---
+++ jgiven-junit/src/main/java/com/tngtech/jgiven/junit/de/SzenarioTest.java
@@ -0,0 +1,17 @@
+package com.tngtech.jgiven.junit.de;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+import com.tngtech.jgiven.junit.ScenarioExecutionRule;
+import com.tngtech.jgiven.junit.ScenarioReportRule;
+import com.tngtech.jgiven.lang.de.SzenarioTestBasis;
+
+public class SzenarioTest<GEGEBEN, WENN, DANN> extends SzenarioTestBasis<GEGEBEN, WENN, DANN> {
+    @ClassRule
+    public static final ScenarioReportRule writerRule = new ScenarioReportRule();
+
+    @Rule
+    public final ScenarioExecutionRule scenarioRule = new ScenarioExecutionRule( writerRule.getTestCaseModel(), this, getScenario() );
+
+}
---
+++ jgiven-junit/src/test/java/com/tngtech/jgiven/junit/de/DeSzenarioTest.java
@@ -0,0 +1,37 @@
+package com.tngtech.jgiven.junit.de;
+
+import org.junit.Test;
+
+import com.tngtech.jgiven.junit.de.DeSzenarioTest.DeutscheTestSchritte;
+import com.tngtech.jgiven.lang.de.Schritte;
+
+public class DeSzenarioTest extends SzenarioTest<DeutscheTestSchritte, DeutscheTestSchritte, DeutscheTestSchritte> {
+
+    @Test
+    public void Szenarien_koennen_in_deutsch_geschrieben_werden() {
+        gegeben().ein_deutsches_Projekt();
+        wenn().JGiven_verwendet_wird()
+            .und().die_Szenarien_in_deutsch_geschrieben_werden();
+        dann().generiert_JGiven_deutsche_Berichte();
+    }
+
+    static class DeutscheTestSchritte extends Schritte<DeutscheTestSchritte> {
+
+        public DeutscheTestSchritte ein_deutsches_Projekt() {
+            return self();
+        }
+
+        public DeutscheTestSchritte generiert_JGiven_deutsche_Berichte() {
+            return self();
+        }
+
+        public DeutscheTestSchritte die_Szenarien_in_deutsch_geschrieben_werden() {
+            return self();
+        }
+
+        public DeutscheTestSchritte JGiven_verwendet_wird() {
+            return self();
+        }
+
+    }
+}
