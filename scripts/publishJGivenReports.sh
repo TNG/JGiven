209dbf0aff424a4dd3ea2296e2cd99eb9b6c0d28
Jan Schäfer <jan.schaefer@tngtech.com>
2014-06-04 10:30:34 +0200

    Remove old report directory when publish jgiven reports

--- scripts/publishJGivenReports.sh
+++ scripts/publishJGivenReports.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git checkout gh-pages
+git rm -r jgiven-report/*
 cp -r jgiven-tests/target/jgiven-reports/html/ jgiven-report
 git add jgiven-report/
 git commit -m 'added generated jgiven report to gh-pages'
