87bd733f2651a37a6bd406814dbb680cacc617f7
Jan Schäfer <jan.schaefer@tngtech.com>
2014-06-15 17:16:07 +0200

    Fix compilation error

--- jgiven-tests/src/main/java/com/tngtech/jgiven/tests/WhenTestStage.java
+++ jgiven-tests/src/main/java/com/tngtech/jgiven/tests/WhenTestStage.java
@@ -1,7 +1,5 @@
 package com.tngtech.jgiven.tests;
 
-import org.assertj.core.api.Assertions;
-
 import com.tngtech.jgiven.Stage;
 
 public class WhenTestStage extends Stage<WhenTestStage> {
@@ -9,7 +7,7 @@ public class WhenTestStage extends Stage<WhenTestStage> {
     public void something_happens() {}
 
     public void a_step_fails() {
-        Assertions.assertThat( true ).as( "An assertion failed" ).isFalse();
+        throw new AssertionError();
     }
 
 }
