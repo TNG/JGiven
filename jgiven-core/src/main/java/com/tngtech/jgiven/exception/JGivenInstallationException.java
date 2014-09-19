90c63ed80143a775f4da490fab6f6c41ca959346
Jan Schäfer <jan.schaefer@tngtech.com>
2014-09-19 15:19:02 +0200

    Add JGiven version information to HTML report

---
+++ jgiven-core/src/main/java/com/tngtech/jgiven/exception/JGivenInstallationException.java
@@ -0,0 +1,12 @@
+package com.tngtech.jgiven.exception;
+
+public class JGivenInstallationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public JGivenInstallationException( String message, Throwable cause ) {
+        super( message + ".\n"
+                + "This is most likely an issue with your installation or your environment setup and not a JGiven defect.",
+            cause );
+    }
+
+}
---
+++ jgiven-core/src/main/java/com/tngtech/jgiven/impl/util/Version.java
@@ -0,0 +1,45 @@
+package com.tngtech.jgiven.impl.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.tngtech.jgiven.exception.JGivenInstallationException;
+
+public class Version {
+    public static final String JGIVEN_VERSION_PROPERTIES = "com/tngtech/jgiven/jgiven-version.properties";
+    public static final Version VERSION = loadVersion();
+
+    private final String versionString;
+    private final String commitHash;
+
+    public Version( String versionString, String commitHash ) {
+        this.versionString = versionString;
+        this.commitHash = commitHash;
+    }
+
+    @Override
+    public String toString() {
+        return versionString + "-" + commitHash;
+    }
+
+    private static Version loadVersion() {
+        Properties properties = new Properties();
+        InputStream resourceAsStream = Version.class.getClassLoader().getResourceAsStream( JGIVEN_VERSION_PROPERTIES );
+        try {
+            properties.load( resourceAsStream );
+            return Version.fromProperties( properties );
+        } catch( Exception e ) {
+            throw new JGivenInstallationException( "Could not load the JGiven version file " + JGIVEN_VERSION_PROPERTIES + ". "
+                    + e.getMessage(), e );
+        } finally {
+            ResourceUtil.close( resourceAsStream );
+        }
+    }
+
+    private static Version fromProperties( Properties properties ) {
+        String versionString = properties.getProperty( "jgiven.version", "Unkown Version" );
+        String commitHash = properties.getProperty( "jgiven.buildNumber", "Unkown Build Number" );
+
+        return new Version( versionString, commitHash );
+    }
+}
