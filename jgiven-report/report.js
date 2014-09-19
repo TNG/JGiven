289adf62021ffd3016e81d8a1053ccd455fb3da3
Jan Schäfer <jan.schaefer@tngtech.com>
2014-09-19 15:33:43 +0200

    added generated jgiven report to gh-pages

--- jgiven-report/report.js
+++ jgiven-report/report.js
@@ -1,9 +1,12 @@
 function toggle(id) {
    var element = document.getElementById(id);
-   console.log(element.style.display);
-   if (element.style.display === 'block') {
-       element.style.display = 'none';
+   if (element.classList.toggle) {
+      element.classList.toggle('collapsed');
    } else {
-       element.style.display = 'block';
+      if (element.style.display === 'block') {
+          element.style.display = 'none';
+      } else {
+          element.style.display = 'block';
+      }
    }
 }
