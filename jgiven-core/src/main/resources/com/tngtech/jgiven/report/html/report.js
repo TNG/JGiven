17cc22a0991acf9b603074bb1cecd3cc7f8fd527
Jan Schäfer <jan.schaefer@tngtech.com>
2014-09-24 14:03:48 +0200

    Highlight matching text in HTML report

--- jgiven-core/src/main/resources/com/tngtech/jgiven/report/html/report.js
+++ jgiven-core/src/main/resources/com/tngtech/jgiven/report/html/report.js
@@ -51,18 +51,27 @@ function searchChanged(event) {
       var search = searchfield.value;
       console.log("Search for " + search);
 
-      openMatchingElements(search, toc, 0);
+      openMatchingElements(new RegExp(search,'i'), toc, 0);
    }, 100);
 }
 
 function openMatchingElements(search, element, depth) {
-   if (element.nodeType==3 && element.nodeValue.match(new RegExp(search,'i') )) {
+   if (element.nodeType==3 && element.nodeValue.match(search )) {
       return true;
    }
 
    if (depth > 1 && (element.tagName === "UL" || element.tagName === "LI") && !isCollapsed(element)) {
       toggleElement(element);
    }
+
+   if (element.tagName === "A") {
+      var originalValue = element.getAttribute("data-original");
+      if (originalValue) {
+         element.innerHTML = originalValue;
+      }
+      element.classList.remove('diff');
+   }
+
 
    var found = false;
    var children = element.childNodes;
@@ -75,10 +84,18 @@ function openMatchingElements(search, element, depth) {
    if (found) {
       if (isCollapsed(element)) {
          toggleElement(element);
-         if (element.tagName === "A") {
-            element.classList.toggle('diff');
-         }
+      }
+      if (element.tagName === "A") {
+         element.setAttribute("data-original", element.innerHTML);
+         element.innerHTML = highlightMatches(element.innerHTML, search);
       }
    }
    return found;
+}
+
+function highlightMatches(text, search) {
+
+   var result = text.replace(search, "<span class='diff'>$&</span>");
+   console.log("Replaced "+result);
+   return result;
 }
