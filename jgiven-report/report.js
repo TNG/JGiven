582032b393837db934586a7aab54e6a76eadd9e5
Jan Schäfer <jan.schaefer@tngtech.com>
2014-09-28 13:25:56 +0200

    added generated jgiven report to gh-pages

--- jgiven-report/report.js
+++ jgiven-report/report.js
@@ -74,7 +74,38 @@ function isCollapsed(element) {
 var searchTimeout;
 
 /**
- * Invoked when the content of the search field changes
+ * Invoked when the content of the content search field changes
+ */
+function contentSearchChanged(event) {
+   if (searchTimeout) {
+      window.clearTimeout(searchTimeout);
+   }
+
+   searchTimeout = window.setTimeout( function() {
+      var toc = document.getElementById('content');
+      var searchfield = document.getElementById('content-search-input');
+      var search = searchfield.value;
+      console.log("Search for " + search);
+      openMatchingElements(new RegExp(search,'i'), toc, 2, contentElementsToToggle, elementsToBeSearched);
+
+      if (search === '') {
+         collapseAll(toc, 1, contentElementsToBeClosed);
+      }
+   }, 100);
+}
+
+function contentElementsToToggle(element) {
+   return element.className === 'scenario-body'
+      || element.className === 'scenario'
+      || element.className === 'case-content';
+}
+
+function tocElementsToToggle(element) {
+   return element.tagName === "UL" || element.tagName === "LI";
+}
+
+/**
+ * Invoked when the content of the TOC search field changes
  */
 function searchChanged(event) {
    if (searchTimeout) {
@@ -83,50 +114,74 @@ function searchChanged(event) {
 
    searchTimeout = window.setTimeout( function() {
       var toc = document.getElementById('toc');
-      var searchfield = document.getElementById('searchfield');
+      var searchfield = document.getElementById('toc-search-input');
       var search = searchfield.value;
       console.log("Search for " + search);
-      openMatchingElements(new RegExp(search,'i'), toc, 0);
+      openMatchingElements(new RegExp(search,'i'), toc, 0, tocElementsToToggle, elementsToBeSearched);
 
       if (search === '') {
-         collapseAllUl(toc, 0);
+         collapseAll(toc, 0, ulElement);
       }
    }, 100);
 }
 
-function collapseAllUl(element, depth) {
-   if (depth > 2 && element.tagName === "UL" && !isCollapsed(element)) {
+function elementsToBeSearched(element) {
+   if (element.className) {
+      return element.className.indexOf('extended-description') === -1;
+   }
+   return true;
+}
+
+function contentElementsToBeClosed(element) {
+   return element.className === 'scenario-body'
+      || element.className === 'case-content';
+}
+
+function ulElement(element) {
+   return element.tagName === "UL";
+}
+
+function collapseAll(element, depth, toBeClosed) {
+   if (depth > 2 && toBeClosed(element) && !isCollapsed(element)) {
       toggleElement(element);
    }
 
    var children = element.childNodes;
    for(var i=0; i < children.length; i++) {
-      collapseAllUl(children[i], depth + 1);
+      collapseAll(children[i], depth + 1, toBeClosed);
    }
 }
 
-function openMatchingElements(search, element, depth) {
+function onlyHasTextNode(element) {
+   return element.childNodes.length === 1
+      && element.childNodes[0].nodeType === 3;
+}
+
+function openMatchingElements(search, element, depth, shouldToggleElement, shouldSearchElement) {
+   if (!shouldSearchElement(element)) {
+      return false;
+   }
+
    if (element.nodeType==3 && element.nodeValue.match(search )) {
       return true;
    }
 
-   if (depth > 2 && (element.tagName === "UL" || element.tagName === "LI") && !isCollapsed(element)) {
+   // close element by default, in case it is currently visible
+   if (depth > 2 && shouldToggleElement(element) && !isCollapsed(element)) {
       toggleElement(element);
    }
 
-   if (element.tagName === "A") {
+   if (element.getAttribute) {
       var originalValue = element.getAttribute("data-original");
       if (originalValue) {
          element.innerHTML = originalValue;
       }
-      element.classList.remove('diff');
    }
-
 
    var found = false;
    var children = element.childNodes;
    for(var i=0; i < children.length; i++) {
-      if (openMatchingElements(search, children[i], depth + 1)) {
+      if (openMatchingElements(search, children[i], depth + 1,shouldToggleElement, shouldSearchElement)) {
          found = true;
       }
    }
@@ -135,7 +190,7 @@ function openMatchingElements(search, element, depth) {
       if (isCollapsed(element)) {
          toggleElement(element);
       }
-      if (element.tagName === "A") {
+      if (onlyHasTextNode(element)) {
          element.setAttribute("data-original", element.innerHTML);
          element.innerHTML = highlightMatches(element.innerHTML, search);
       }
@@ -144,8 +199,10 @@ function openMatchingElements(search, element, depth) {
 }
 
 function highlightMatches(text, search) {
-
-   var result = text.replace(search, "<span class='diff'>$&</span>");
-   console.log("Replaced "+result);
+   var result = text.replace(search, "<span class='highlight'>$&</span>");
    return result;
+}
+
+function showExtendedDescription(id) {
+   toggle(id);
 }
