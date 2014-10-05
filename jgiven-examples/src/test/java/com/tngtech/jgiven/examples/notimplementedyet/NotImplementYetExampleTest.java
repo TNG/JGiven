b550bc8e0fb73b8b24f3a7bbada899397aed6b7a
Jan Schäfer <jan.schaefer@tngtech.com>
2014-10-05 13:38:17 +0200

    remove accidentally commited test

--- jgiven-examples/src/test/java/com/tngtech/jgiven/examples/notimplementedyet/NotImplementYetExampleTest.java
+++ jgiven-examples/src/test/java/com/tngtech/jgiven/examples/notimplementedyet/NotImplementYetExampleTest.java
@@ -1,6 +1,5 @@
 package com.tngtech.jgiven.examples.notimplementedyet;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import com.tngtech.jgiven.annotation.Description;
@@ -12,17 +11,6 @@ import com.tngtech.jgiven.tags.FeatureNotImplementedYet;
         + "I want to write my scenarios before I start coding<br>"
         + "In order to discuss them with business stakeholders" )
 public class NotImplementYetExampleTest extends SimpleScenarioTest<NotImplementYetExampleTest.TestSteps> {
-
-    @Test
-    @FeatureNotImplementedYet
-    // @NotImplementedYet
-    public void tests_that_are_not_implemented_yet_can_be_annotated_with_the_NotImplementedYet_annotation() {
-        given().some_state();
-        when().some_action();
-        then().some_result();
-
-        Assertions.assertThat( true ).isFalse();
-    }
 
     @Test
     @FeatureNotImplementedYet
