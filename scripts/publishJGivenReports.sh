53eb6d1767abf751966c0f63685abee7a0733866
Jan Schäfer <jan.schaefer@tngtech.com>
2014-11-29 23:47:55 +0100

    update publishJGivenReports.sh

--- scripts/publishJGivenReports.sh
+++ scripts/publishJGivenReports.sh
@@ -6,8 +6,8 @@ git pull --rebase
 git rm -r jgiven-report/*
 mkdir -p jgiven-report/html
 mkdir -p jgiven-report/html5
-cp -r jgiven-tests/build/reports/jgiven/html/ jgiven-report/html
-cp -r jgiven-tests/build/reports/jgiven/html5/ jgiven-report/html5
+cp -r jgiven-tests/build/reports/jgiven/html/ jgiven-report/
+cp -r jgiven-tests/build/reports/jgiven/html5/ jgiven-report/
 git add jgiven-report/
 git commit -m 'added generated jgiven report to gh-pages'
 git push
