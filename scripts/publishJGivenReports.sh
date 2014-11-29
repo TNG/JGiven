69574c2d80aad895778828fbd12765c6adaeaabe
Jan Schäfer <jan.schaefer@tngtech.com>
2014-11-29 23:41:46 +0100

    update publishJGivenReports.sh

--- scripts/publishJGivenReports.sh
+++ scripts/publishJGivenReports.sh
@@ -4,8 +4,8 @@ gradle clean build
 git checkout gh-pages
 git pull --rebase
 git rm -r jgiven-report/*
-mkdir jgiven-report/html
-mkdir jgiven-report/html5
+mkdir -p jgiven-report/html
+mkdir -p jgiven-report/html5
 cp -r jgiven-tests/build/reports/jgiven/html/ jgiven-report/html
 cp -r jgiven-tests/build/reports/jgiven/html5/ jgiven-report/html5
 git add jgiven-report/
