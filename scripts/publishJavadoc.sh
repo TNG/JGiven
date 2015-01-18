f2bc931ecedf947b25327a65cd2bcaff94af25a6
Jan Schäfer <jan.schaefer@tngtech.com>
2015-01-18 13:07:22 +0100

    create javadoc folder when publishing javadoc

--- scripts/publishJavadoc.sh
+++ scripts/publishJavadoc.sh
@@ -4,6 +4,7 @@ set -ex
 git checkout gh-pages
 git pull --rebase
 git rm -r javadoc/*
+mkdir -p javadoc
 cp -r jgiven-core/build/docs/javadoc/* javadoc
 git add javadoc
 git commit -m 'added javadoc to gh-pages'
