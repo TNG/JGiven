30574dca9f5b2cc96ed53b68a34bc9db2581db8c
Jan Schäfer <jan.schaefer@tngtech.com>
2015-06-21 11:57:32 +0200

    add publishJGivenSnapshotReports.sh

---
+++ scripts/publishJGivenSnapshotReports.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+./gradlew clean build
+git checkout gh-pages
+git pull --rebase
+TARGET_DIR=snapshot/jgiven-report
+git rm -rf ${TARGET_DIR}/*
+mkdir -p ${TARGET_DIR}/html
+mkdir -p ${TARGET_DIR}/html5
+cp -r jgiven-tests/build/reports/jgiven/html/ ${TARGET_DIR}
+cp -r jgiven-tests/build/reports/jgiven/html5/ ${TARGET_DIR}
+git add ${TARGET_DIR}
+git commit -m 'added generated snapshot jgiven report to gh-pages'
+git push
+git checkout -f master
