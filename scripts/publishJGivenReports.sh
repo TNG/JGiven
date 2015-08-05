d39bc80844c4697e4d6a5a19585e546a77afcac9
Jan Schäfer <jan.schaefer@tngtech.com>
2015-08-05 22:38:36 +0200

    adapt publish scripts to not publish static html report anymore

--- scripts/publishJGivenReports.sh
+++ scripts/publishJGivenReports.sh
@@ -4,9 +4,7 @@ set -e
 git checkout gh-pages
 git pull --rebase
 git rm -r jgiven-report/*
-mkdir -p jgiven-report/html
 mkdir -p jgiven-report/html5
-cp -r jgiven-tests/build/reports/jgiven/html/ jgiven-report/
 cp -r jgiven-tests/build/reports/jgiven/html5/ jgiven-report/
 git add jgiven-report/
 git commit -m 'added generated jgiven report to gh-pages'
--- scripts/publishJGivenSnapshotReports.sh
+++ scripts/publishJGivenSnapshotReports.sh
@@ -5,9 +5,7 @@ git checkout gh-pages
 git pull --rebase
 TARGET_DIR=snapshot/jgiven-report
 git rm -rf ${TARGET_DIR}/*
-mkdir -p ${TARGET_DIR}/html
 mkdir -p ${TARGET_DIR}/html5
-cp -r jgiven-tests/build/reports/jgiven/html/ ${TARGET_DIR}
 cp -r jgiven-tests/build/reports/jgiven/html5/ ${TARGET_DIR}
 git add ${TARGET_DIR}
 git commit -m 'added generated snapshot jgiven report to gh-pages'
