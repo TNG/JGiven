5ff85e92fe7eb4d6ebe989f9dc33d9a3a586d5ec
Jan Schäfer <jan.schaefer@tngtech.com>
2015-09-26 23:34:31 +0200

    cache configuration instances

--- jgiven-core/src/main/java/com/tngtech/jgiven/config/ConfigurationUtil.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/config/ConfigurationUtil.java
@@ -1,9 +1,25 @@
 package com.tngtech.jgiven.config;
 
+import java.util.concurrent.ExecutionException;
+
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.tngtech.jgiven.annotation.JGivenConfiguration;
 import com.tngtech.jgiven.impl.util.ReflectionUtil;
 
 public class ConfigurationUtil {
+
+    private static final LoadingCache<Class<?>, AbstractJGivenConfiguration> configurations = CacheBuilder.newBuilder().build(
+        new CacheLoader<Class<?>, AbstractJGivenConfiguration>() {
+            @Override
+            public AbstractJGivenConfiguration load( Class<?> key ) throws Exception {
+                AbstractJGivenConfiguration result = (AbstractJGivenConfiguration) ReflectionUtil.newInstance( key );
+                result.configure();
+                return result;
+            }
+        } );
 
     public static AbstractJGivenConfiguration getConfiguration( Class<? extends Object> testClass ) {
         JGivenConfiguration annotation = testClass.getAnnotation( JGivenConfiguration.class );
@@ -11,9 +27,10 @@ public class ConfigurationUtil {
             return new DefaultConfiguration();
         }
 
-        AbstractJGivenConfiguration result = ReflectionUtil.newInstance( annotation.value() );
-        result.configure();
-        return result;
+        try {
+            return configurations.get( annotation.value() );
+        } catch( ExecutionException e ) {
+            throw Throwables.propagate( e.getCause() );
+        }
     }
-
 }
