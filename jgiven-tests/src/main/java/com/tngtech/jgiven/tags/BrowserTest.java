7e80a73a199c88cbbed683022813f0fd701c3c41
Jan Schäfer <jan.schaefer@tngtech.com>
2015-09-28 21:29:38 +0200

    change tag color

--- jgiven-tests/src/main/java/com/tngtech/jgiven/tags/BrowserTest.java
+++ jgiven-tests/src/main/java/com/tngtech/jgiven/tags/BrowserTest.java
@@ -7,7 +7,7 @@ import com.tngtech.jgiven.annotation.IsTag;
 
 @IsTag(
     description = "Tests with this tag use a browser for testing",
-    color = "red" )
+    color = "rgb(69, 158, 19)" )
 @Retention( RetentionPolicy.RUNTIME )
 public @interface BrowserTest {
 
