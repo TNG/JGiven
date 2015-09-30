d0e65874c6416a9a288276a04f3a13df0efecd8e
Jan Schäfer <jan.schaefer@tngtech.com>
2015-09-30 09:07:02 +0200

    added generated snapshot jgiven report to gh-pages

---
+++ snapshot/jgiven-report/html5/lib/api.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Global variable that is used by the generated JSONP files.
+ * This variable can also be used by the custom Javascript file.
+ */
+var jgivenReport = {
+  scenarios: [],
+  customNavigationLinks: [],
+
+  setTags: function setTags (tagFile) {
+    this.tagFile = tagFile;
+  },
+
+  setMetaData: function setMetaData (metaData) {
+    this.metaData = metaData;
+    _.forEach(metaData.data, function (x) {
+      document.writeln("<script src='data/" + x + "'></script>");
+    });
+  },
+
+  addScenarios: function addScenarios (scenarios) {
+    this.scenarios = this.scenarios.concat(scenarios);
+  },
+
+  setAllScenarios: function setAllScenarios (allScenarios) {
+    this.scenarios = allScenarios;
+  },
+
+  addNavigationLink: function addNavigationLink (link) {
+    this.customNavigationLinks.push(link);
+  },
+
+  setTitle: function setTitle (title) {
+    this.metaData.title = title;
+  }
+};
+
