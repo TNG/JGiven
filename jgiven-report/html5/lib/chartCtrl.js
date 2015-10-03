284554c7519cea10038d0637bf7619a44887bc9a
Jan Schäfer <jan.schaefer@tngtech.com>
2015-10-03 11:42:44 +0200

    added generated jgiven report to gh-pages

--- jgiven-report/html5/lib/chartCtrl.js
+++ jgiven-report/html5/lib/chartCtrl.js
@@ -1,5 +1,4 @@
-
-jgivenReportApp.controller('ChartCtrl', function ($scope) {
+jgivenReportApp.controller('ChartCtrl', function ($scope, $location) {
   var red = Chart.defaults.global.colours[2];
   var blue = Chart.defaults.global.colours[0];
   var green = {
@@ -12,6 +11,16 @@ jgivenReportApp.controller('ChartCtrl', function ($scope) {
   };
   var gray = Chart.defaults.global.colours[6];
 
+  $scope.click = function (event) {
+    if (event[0].label === 'Failed') {
+      $scope.showFailed();
+    } else if (event[0].label === 'Pending') {
+      $scope.showPending();
+    } else {
+      $scope.showSuccessful();
+    }
+  };
+
   $scope.labels = ['Successful', 'Failed', 'Pending'];
   $scope.colours = [green, red, gray];
   $scope.options = {
@@ -21,5 +30,6 @@ jgivenReportApp.controller('ChartCtrl', function ($scope) {
     segmentShowStroke: false
   };
 
-});
+})
+;
 
