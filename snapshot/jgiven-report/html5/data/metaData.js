d9a2b96a3aa9c9d364db1823a0f9a3bc4e8f28d4
Jan Schäfer <jan.schaefer@tngtech.com>
2015-12-20 17:38:13 +0100

    added generated snapshot jgiven report to gh-pages

--- snapshot/jgiven-report/html5/data/metaData.js
+++ snapshot/jgiven-report/html5/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Dec 20, 2015 5:21:55 PM","title":"JGiven Report","data":["data0.js"]} );
+jgivenReport.setMetaData({"created":"Dec 20, 2015 5:38:09 PM","title":"JGiven Report","data":["data0.js"]} );
