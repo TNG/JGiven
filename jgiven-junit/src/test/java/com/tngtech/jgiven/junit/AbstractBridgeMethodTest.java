f08b7496be23e245f6f3d0c35a6c27fe6daf0edd
Jan Schäfer <jan.schaefer@tngtech.com>
2016-03-20 08:28:22 +0100

    Wrote test to reproduce issue with annotations and bridge methods #193

--- jgiven-junit/src/test/java/com/tngtech/jgiven/junit/AbstractBridgeMethodTest.java
+++ jgiven-junit/src/test/java/com/tngtech/jgiven/junit/AbstractBridgeMethodTest.java
@@ -2,8 +2,11 @@ package com.tngtech.jgiven.junit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Assume;
 import org.junit.Test;
 
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.Quoted;
 import com.tngtech.jgiven.junit.ScenarioExecutionTest.TestStage;
 
 public abstract class AbstractBridgeMethodTest<T extends AbstractBridgeMethodTest.BridgeMethodTestStage> extends
@@ -16,10 +19,23 @@ public abstract class AbstractBridgeMethodTest<T extends AbstractBridgeMethodTes
         assertThat( getScenario().getScenarioCaseModel().getSteps() ).isNotEmpty();
     }
 
-    static class BridgeMethodTestStage {
+    @Test
+    public void annotations_of_bridge_methods_are_correctly_read() {
+        String version = System.getProperty( "java.version" );
+        Assume.assumeFalse( "This works only since Java 8", version.startsWith( "1.6" ) || version.startsWith( "1.7" ) );
+
+        given().method_with_formatter_$( "foo" );
+
+        assertThat( getScenario().getScenarioCaseModel().getSteps().get( 0 ).getWord( 2 ).getFormattedValue() ).isEqualTo( "\"foo\"" );
+    }
+
+    static abstract class BridgeMethodTestStage<SELF extends BridgeMethodTestStage<?>> extends Stage<SELF> {
         BridgeMethodTestStage method_that_is_overidden_with_different_return_type() {
             return this;
         }
+
+        abstract SELF method_with_formatter_$( @Quoted String text );
+
     }
 
 }
--- jgiven-junit/src/test/java/com/tngtech/jgiven/junit/BridgeMethodTest.java
+++ jgiven-junit/src/test/java/com/tngtech/jgiven/junit/BridgeMethodTest.java
@@ -1,11 +1,20 @@
 package com.tngtech.jgiven.junit;
 
+import com.tngtech.jgiven.annotation.Quoted;
+
 public class BridgeMethodTest extends AbstractBridgeMethodTest<BridgeMethodTest.SubclassBridgeMethodTestStage> {
 
-    static class SubclassBridgeMethodTestStage extends AbstractBridgeMethodTest.BridgeMethodTestStage {
+    static class SubclassBridgeMethodTestStage<SELF extends SubclassBridgeMethodTestStage<?>> extends
+            AbstractBridgeMethodTest.BridgeMethodTestStage<SELF> {
         @Override
         SubclassBridgeMethodTestStage method_that_is_overidden_with_different_return_type() {
             return this;
         }
+
+        @Override
+        SELF method_with_formatter_$( @Quoted String text ) {
+            return self();
+        }
+
     }
 }
