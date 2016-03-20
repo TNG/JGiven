13eb0d1e783023bb88e95385fbdc1506919a92a4
Jan Schäfer <jan.schaefer@tngtech.com>
2016-03-20 08:27:39 +0100

    Add example to reproduce issue with @Pending annotation and multiple cases #200

--- jgiven-examples/src/test/java/com/tngtech/jgiven/examples/pending/PendingExampleTest.java
+++ jgiven-examples/src/test/java/com/tngtech/jgiven/examples/pending/PendingExampleTest.java
@@ -1,19 +1,23 @@
-package com.tngtech.jgiven.examples.notimplementedyet;
+package com.tngtech.jgiven.examples.pending;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.jgiven.annotation.Description;
 import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.junit.SimpleScenarioTest;
 import com.tngtech.jgiven.tags.FeaturePending;
 
+@RunWith( DataProviderRunner.class )
+@FeaturePending
 @Description( "As a good BDD practitioner,<br>"
         + "I want to write my scenarios before I start coding<br>"
         + "In order to discuss them with business stakeholders" )
 public class PendingExampleTest extends SimpleScenarioTest<PendingExampleTest.TestSteps> {
 
     @Test
-    @FeaturePending
     @Pending
     public void scenarios_that_are_pending_can_be_annotated_with_the_Pending_annotation() {
         given().some_state();
@@ -22,10 +26,18 @@ public class PendingExampleTest extends SimpleScenarioTest<PendingExampleTest.Te
     }
 
     @Test
-    @FeaturePending
     public void single_steps_can_be_annotated_with_Pending() {
         given().some_state();
         when().some_pending_action();
+        then().some_result();
+    }
+
+    @Test
+    @Pending
+    @DataProvider( { "1st", "2nd" } )
+    public void multiple_cases_can_be_pending( String actionCount ) {
+        given().some_state();
+        when().a_$_action( actionCount );
         then().some_result();
     }
 
@@ -40,6 +52,10 @@ public class PendingExampleTest extends SimpleScenarioTest<PendingExampleTest.Te
         }
 
         public TestSteps some_action() {
+            return this;
+        }
+
+        public TestSteps a_$_action( String actionCount ) {
             return this;
         }
 
