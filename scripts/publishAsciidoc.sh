9eccb9970fce48e555065027430e2f9291eed39f
Jan Schäfer <jan.schaefer@tngtech.com>
2016-08-26 14:33:45 +0200

    add script to publish asciidoc #230

---
+++ scripts/publishAsciidoc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ex
+./gradlew asciidoctor
+git checkout gh-pages
+git pull --rebase
+#git rm -fr userguide/*
+mkdir -p userguide
+cp -r build/asciidoc/html5/* userguide
+git add userguide
+git commit -m 'added userguide to gh-pages'
+git push
+git checkout -f master
