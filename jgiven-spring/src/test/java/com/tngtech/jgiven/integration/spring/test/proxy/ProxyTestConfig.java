e54f8263ca5f6729bcea0fc1032e33b3d4378a9c
Jan Schäfer <jan.schaefer@tngtech.com>
2016-12-21 08:52:45 +0100

    Add test case to test Spring @Transaction annotation on Stage classes (#254)

--- jgiven-spring/src/test/java/com/tngtech/jgiven/integration/spring/test/proxy/ProxyTestConfig.java
+++ jgiven-spring/src/test/java/com/tngtech/jgiven/integration/spring/test/proxy/ProxyTestConfig.java
@@ -7,10 +7,27 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @EnableJGiven
 @ComponentScan( basePackages = "com.tngtech.jgiven.integration.spring.test.proxy" )
 @Import(TestAspect.class)
+@EnableTransactionManagement
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class ProxyTestConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
 }
