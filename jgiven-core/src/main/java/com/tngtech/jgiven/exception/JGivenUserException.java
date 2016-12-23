b8bf0d2aad414d58996b2afe87a666f16c20cde4
Ingo Bürk <ingo.buerk@tngtech.com>
2016-12-23 09:42:47 +0100

    Fixed minor typo in JavaDoc.

--- jgiven-core/src/main/java/com/tngtech/jgiven/exception/JGivenUserException.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/exception/JGivenUserException.java
@@ -3,7 +3,7 @@ package com.tngtech.jgiven.exception;
 import java.lang.reflect.Method;
 
 /**
- * This exception is thrown when JGiven tried to execute a used defined method and that method has thrown an exception.
+ * This exception is thrown when JGiven tried to execute a user-defined method and that method has thrown an exception.
  */
 public class JGivenUserException extends RuntimeException {
     private static final long serialVersionUID = 1L;
