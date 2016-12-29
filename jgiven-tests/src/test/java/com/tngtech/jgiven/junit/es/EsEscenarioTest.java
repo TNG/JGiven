3d2e70f55a076e550b1622039fc012c41fce23fe
Elena García Herrero <Elena Garca Herrero>
2016-12-29 11:16:53 +0100

    rename paso to fase

--- jgiven-tests/src/test/java/com/tngtech/jgiven/junit/es/EsEscenarioTest.java
+++ jgiven-tests/src/test/java/com/tngtech/jgiven/junit/es/EsEscenarioTest.java
@@ -2,12 +2,12 @@ package com.tngtech.jgiven.junit.es;
 
 import org.junit.Test;
 
-import com.tngtech.jgiven.junit.es.EsEscenarioTest.PasoEspannolTest;
+import com.tngtech.jgiven.junit.es.EsEscenarioTest.FaseEspannolTest;
 import com.tngtech.jgiven.lang.es.Fase;
 import com.tngtech.jgiven.tags.FeatureSpanish;
 
 @FeatureSpanish
-public class EsEscenarioTest extends EscenarioTest<PasoEspannolTest, PasoEspannolTest, PasoEspannolTest> {
+public class EsEscenarioTest extends EscenarioTest<FaseEspannolTest, FaseEspannolTest, FaseEspannolTest> {
 
     @Test
     public void los_escenarios_se_pueden_escribir_en_espannol() {
@@ -21,21 +21,21 @@ public class EsEscenarioTest extends EscenarioTest<PasoEspannolTest, PasoEspanno
 
     }
 
-    static class PasoEspannolTest extends Fase<PasoEspannolTest> {
+    static class FaseEspannolTest extends Fase<FaseEspannolTest> {
 
-        public PasoEspannolTest un_proyecto_en_espannol() {
+        public FaseEspannolTest un_proyecto_en_espannol() {
             return self();
         }
 
-        public PasoEspannolTest se_usa_JGiven() {
+        public FaseEspannolTest se_usa_JGiven() {
         	return self();
         }
 
-        public PasoEspannolTest los_escenarios_se_escriben_en_espannol() {
+        public FaseEspannolTest los_escenarios_se_escriben_en_espannol() {
         	return self();
         }
 
-        public PasoEspannolTest JGiven_genera_los_informes_en_espannol() {
+        public FaseEspannolTest JGiven_genera_los_informes_en_espannol() {
             return self();
         }
     }
