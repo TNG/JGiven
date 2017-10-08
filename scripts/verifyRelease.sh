2c06dcf874a0693c2d56435e6c617155d1fbd097
Jan Schäfer <jan.schaefer@tngtech.com>
2017-10-08 15:52:25 +0200

    improve verifyRelease script

--- scripts/verifyRelease.sh
+++ scripts/verifyRelease.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 
 echo "Testing Maven plugin..."
-mvn -f example-projects/maven/pom.xml clean test -Djgiven.version=$VERSION
+mvn -U -f example-projects/maven/pom.xml clean test -Djgiven.version=$VERSION
 
 echo "Testing Gradle plugin..."
 ./gradlew -b example-projects/junit5/build.gradle clean test -Pversion=$VERSION
