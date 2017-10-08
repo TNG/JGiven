4da0caaf8ef417539f6530a7065e199a2cb1a843
Jan Schäfer <jan.schaefer@tngtech.com>
2017-10-08 13:37:30 +0200

    add script to verify release

---
+++ scripts/verifyRelease.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=$1
+
+echo "Testing Maven plugin..."
+mvn -f example-projects/maven/pom.xml clean test -Djgiven.version=$VERSION
+
+echo "Testing Gradle plugin..."
+./gradlew -b example-projects/junit5/build.gradle clean test -Pversion=$VERSION
+
+
