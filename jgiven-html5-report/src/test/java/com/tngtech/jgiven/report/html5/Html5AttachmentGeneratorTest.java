4ea6c0093aabb1496b3a5ee8c5acb707e09672ca
Luca Maccione <luca.maccione@tngtech.com>
2017-10-17 08:24:15 +0200

    Fix IllegalArgumentException if size of thumbnail is rounded to 0

--- jgiven-html5-report/src/test/java/com/tngtech/jgiven/report/html5/Html5AttachmentGeneratorTest.java
+++ jgiven-html5-report/src/test/java/com/tngtech/jgiven/report/html5/Html5AttachmentGeneratorTest.java
@@ -1,17 +1,32 @@
 package com.tngtech.jgiven.report.html5;
 
+import static com.tngtech.jgiven.report.html5.Html5AttachmentGenerator.MINIMAL_THUMBNAIL_SIZE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.imageio.ImageIO;
+
+import org.assertj.core.util.Lists;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import com.google.common.io.BaseEncoding;
+import com.tngtech.jgiven.attachment.Attachment;
+import com.tngtech.jgiven.attachment.MediaType;
+import com.tngtech.jgiven.report.model.StepModel;
+import com.tngtech.jgiven.report.model.Word;
 
 public class Html5AttachmentGeneratorTest {
 
     private static final String JSON_SAMPLE = "{}";
-    private static final byte[] BINARY_SAMPLE = BaseEncoding.base64().decode( "89504E470D0A1A0A" );
+    private static final byte[] BINARY_SAMPLE = BaseEncoding.base64().decode(
+        "R0lGODlhFgAWAOZiAAUFBd3d3eHh4be3tzg4ODU1NaysrMHBwTs7O39/f15eXs/Pz+fn5wwMDEVFRTIyMh0dHUpKSigoKJeXl3x8fKioqOTk5NPT05qamjc3N2RkZDExMYmJiUxMTDo6Ouvr62pqanFxcW1tbfz8/CAgIM3Nze7u7vn5+eLi4m5ubjMzM52dnRcXF5ubm/Pz801NTcvLyz09PSwsLISEhJmZmT8/P+Xl5SIiIggICJGRkVxcXCUlJfDw8K2trZaWlhAQED4+Pmtra2hoaMfHx0BAQFpaWsrKynl5eVZWVq+vr6Ojo/j4+IeHh3p6ehQUFNjY2Ly8vK6ursLCwmBgYJSUlHBwcICAgIODg/Hx8WJiYouLi9/f33JycsTExC4uLvb29v///wICAi4uLgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAGIALAAAAAAWABYAAAf/gGKCgwVFPgFfXwEtUwWDj4MbIQJglQxblWACKV6QggQ9YCcTDmGmYQVaX2BKHpAEMGADJKe1YRJGYAsIkVFgKwC2tgBXYAcPglyywcLCVGBNYgUCLhCnDwi1LFkNpjgoAh4KYDmnMpUJpjcWYBOnGmBBNGAEpySrYAk77GAcp04jBgTgYSsCPiyVDDAzVQLRE2ERlmRSaCtJogvCJHzIZEWYAUUfbElgUGkEOltdAlQAY83UD5JgDHTAB+IUABMDhIARcYpAwmAGwVQ45QBMFQICbHQzpQHEwgIJWoaBIgCImCP9mjUbx0TQgwNgFGitheDEgQyDiCwAQ2FsGB1fKxZ0gFQD7AUkS4lKMfbCkxgVFCh9KYEBwxATmmag9SsIgQiBiQIMCBHDUyAAOw==" );
 
     @Rule
     public final TemporaryFolder temporaryFolderRule = new TemporaryFolder();
@@ -26,6 +41,25 @@ public class Html5AttachmentGeneratorTest {
         assertThat( generator.getTargetFile( "foo4", "png" ).getName() ).isEqualTo( "foo4.png" );
         assertThat( generator.getTargetFile( "foo", "png" ).getName() ).isEqualTo( "foo5.png" );
 
+    }
+
+    @Test
+    public void testScalingOfImageToMinimumSize() throws IOException, URISyntaxException {
+        Attachment attachment = Attachment.fromBinaryBytes( BINARY_SAMPLE, MediaType.GIF );
+        BufferedImage before = ImageIO.read( new ByteArrayInputStream( BaseEncoding.base64().decode( attachment.getContent() ) ) );
+        assertThat( before.getWidth() ).isEqualTo( 22 );
+        assertThat( before.getHeight() ).isEqualTo( 22 );
+
+        Html5AttachmentGenerator generator = new Html5AttachmentGenerator( temporaryFolderRule.getRoot() );
+        StepModel stepModel = new StepModel( "test", Lists.<Word>newArrayList() );
+        stepModel.addAttachment( attachment );
+        generator.visit( stepModel );
+
+        File writtenFile = new File( temporaryFolderRule.getRoot().getPath() + "/attachment-thumb.gif" );
+        Attachment writtenAttachment = Attachment.fromBinaryFile( writtenFile, MediaType.GIF );
+        BufferedImage after = ImageIO.read( new ByteArrayInputStream( BaseEncoding.base64().decode( writtenAttachment.getContent() ) ) );
+        assertThat( after.getWidth() ).isEqualTo( MINIMAL_THUMBNAIL_SIZE );
+        assertThat( after.getHeight() ).isEqualTo( MINIMAL_THUMBNAIL_SIZE );
     }
 
 }
