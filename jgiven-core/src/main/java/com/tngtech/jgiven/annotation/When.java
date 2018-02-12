1849ba03d6c43bd2b52914d08b8ccbd1999324a5
nikowitt <github@wittenbeck-it.de>
2018-02-12 13:44:16 +0100

    added marker annotations

---
+++ jgiven-core/src/main/java/com/tngtech/jgiven/annotation/When.java
@@ -0,0 +1,18 @@
+package com.tngtech.jgiven.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marker annotation to indicate that a method is used in when.
+ * Is not used programmatically for the time being.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface When {
+
+}
