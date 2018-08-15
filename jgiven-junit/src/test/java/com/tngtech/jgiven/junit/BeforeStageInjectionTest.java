ffc9fe135aee004c9c14816b12285a8adab95276
Jan Schäfer <jan.schaefer@tngtech.com>
2018-08-15 22:42:23 +0200

    add test case to reproduce #364

---
+++ jgiven-junit/src/test/java/com/tngtech/jgiven/junit/BeforeStageInjectionTest.java
@@ -0,0 +1,36 @@
+package com.tngtech.jgiven.junit;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.tngtech.jgiven.annotation.BeforeStage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+
+public class BeforeStageInjectionTest extends SimpleScenarioTest<BeforeStageInjectionTest.BeforeStageInjectionStage> {
+
+    @Rule
+    @ProvidedScenarioState
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testAfterScenario( ) {
+        given().someStep();
+    }
+
+    public static class BeforeStageInjectionStage {
+        @ExpectedScenarioState
+        TemporaryFolder folder;
+
+        @BeforeStage
+        public void init() {
+            Assertions.assertThat(folder).isNotNull();
+        }
+
+        void someStep() {
+            Assertions.assertThat(folder).isNotNull();
+        }
+    }
+}
