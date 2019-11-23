cd55b45c3f5b90edf20541b7f167f550af0d1c58
javier.salinas <javier.salinas@zopa.com>
2019-11-24 00:13:19 +0100

    Use byte buddy TypeCache instead of custom cache.

--- jgiven-core/src/main/java/com/tngtech/jgiven/impl/CachingStageClassCreator.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/impl/CachingStageClassCreator.java
@@ -1,8 +1,6 @@
 package com.tngtech.jgiven.impl;
 
-import java.util.HashMap;
-import java.util.WeakHashMap;
-import java.util.concurrent.ConcurrentHashMap;
+import net.bytebuddy.TypeCache;
 
 /**
  * Caches stage classes
@@ -10,27 +8,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CachingStageClassCreator implements StageClassCreator {
 
     private StageClassCreator stageCreator;
+    private static TypeCache<Class<?>> typeCache = new TypeCache<>( TypeCache.Sort.SOFT );
 
-    private final static ConcurrentHashMap<Class<?>, Class<?>> createdStages = new ConcurrentHashMap<Class<?>, Class<?>>( 50 );
-
-    public CachingStageClassCreator( StageClassCreator stageCreator ) {
+    public CachingStageClassCreator( StageClassCreator stageCreator ){
         this.stageCreator = stageCreator;
     }
 
     @Override
-    public <T> Class<? extends T> createStageClass( Class<T> stageClass ) {
-        return getCachedClass(stageClass, stageCreator);
-    }
-
-    private static <T> Class<? extends T> getCachedClass(Class<T> stageClass, StageClassCreator stageCreator) {
-        Class<?> stageSubClass = createdStages.get( stageClass );
-        if( stageSubClass == null ) {
-            synchronized (createdStages) {
-                stageSubClass = stageCreator.createStageClass(stageClass);
-                createdStages.put(stageClass, stageSubClass);
-            }
-        }
-
-        return (Class<? extends T>) stageSubClass;
+    @SuppressWarnings("unchecked")
+    public <T> Class<? extends T> createStageClass( Class<T> stageClass ){
+        return (Class<? extends T>) typeCache.findOrInsert( stageClass.getClassLoader(), stageClass,
+                () -> stageCreator.createStageClass( stageClass ) );
     }
 }
