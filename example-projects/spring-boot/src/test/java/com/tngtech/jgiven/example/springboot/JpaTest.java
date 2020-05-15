40430a2cf32ffeef697da96c33c70b459499c7f5
Johannes Leimer <johannes.leimer@tngtech.com>
2020-05-15 15:59:31 +0200

    #415 clean up imports

--- example-projects/spring-boot/src/test/java/com/tngtech/jgiven/example/springboot/JpaTest.java
+++ example-projects/spring-boot/src/test/java/com/tngtech/jgiven/example/springboot/JpaTest.java
@@ -1,15 +1,10 @@
 package com.tngtech.jgiven.example.springboot;
 
 import org.junit.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.annotation.JGivenConfiguration;
 import com.tngtech.jgiven.integration.spring.SimpleSpringRuleScenarioTest;
 
@@ -22,6 +17,5 @@ public class JpaTest extends SimpleSpringRuleScenarioTest<JpaStage> {
     public void test_entity_manager_can_be_used() throws Exception {
         given().test_entity_manager_is_defined();
     }
-
 
 }
