5e4b7cfccac4948e91dbb88a62a40539618885ab
Christian Oertel <christian.oertel@tngtech.com>
2020-11-05 17:46:27 +0100

    removed methods in release.sh for testing
    Signed-off-by: fudler <christian.oertel@tngtech.com>

--- scripts/release_functions.sh
+++ scripts/release_functions.sh
@@ -12,11 +12,11 @@ function find_gradle_property(){
 
 function releaseRepositoryAndPushVersion()
 {
-  echo Releasing the repository...
-  ./gradlew releaseRepository || return 21
+  #echo Releasing the repository...
+  #./gradlew releaseRepository || return 21
 
-  echo Testing Maven plugin from Maven repository...
-  mvn -f example-projects/maven/pom.xml clean test -Djgiven.version="${VERSION}" || return 22
+  #echo Testing Maven plugin from Maven repository...
+  #mvn -f example-projects/maven/pom.xml clean test -Djgiven.version="${VERSION}" || return 22
 
   echo Publishing Gradle Plugin to Gradle Plugin repository...
   ./gradlew -b jgiven-gradle-plugin/build.gradle publishPlugins -Pgradle.publish.key="${GRADLE_PLUGIN_RELEASE_KEY}" -Pgradle.publish.secret="${GRADLE_PLUGIN_RELEASE_SECRET}" || return 23
@@ -24,7 +24,7 @@ function releaseRepositoryAndPushVersion()
   echo Testing Gradle Plugin from Gradle Plugin repository...
   ./gradlew -b example-projects/java9/build.gradle clean test -Pversion="${VERSION}" || return 24
 
-  echo Pushing version and tag to GitHub repository...
-  git push
-  git push "$(git config --get remote.origin.url)" "${VERSION_PREFIXED}" || return 25
+  #echo Pushing version and tag to GitHub repository...
+  #git push
+  #git push "$(git config --get remote.origin.url)" "${VERSION_PREFIXED}" || return 25
 }
