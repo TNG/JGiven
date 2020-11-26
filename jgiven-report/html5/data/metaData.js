af1193808e7236badc4f599c065172db114ccd1c
l-1squared <30831153+l-1squared@users.noreply.github.com>
2020-11-26 07:36:41 +0100

    added generated jgiven report to gh-pages

--- jgiven-report/html5/data/metaData.js
+++ jgiven-report/html5/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Oct 30, 2020, 4:33:29 PM","version":"1.0.0-RC5-bfcbacc6","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
+jgivenReport.setMetaData({"created":"Nov 26, 2020, 7:36:35 AM","version":"1.0.0-RC7-a8747754","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
