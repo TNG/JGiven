5f77c2490f702c68f5290600f4d241e429c6d1c6
Christian Oertel <christian.oertel@tngtech.com>
2021-01-29 08:32:54 +0100

    Changed naming
    Signed-off-by: Christian Oertel <christian.oertel@tngtech.com>

--- example-projects/spock/src/test/groovy/com/tngtech/jgiven/spock/MyShinyJGivenShould.groovy
+++ example-projects/spock/src/test/groovy/com/tngtech/jgiven/spock/MyShinyJGivenShould.groovy
@@ -1,6 +1,12 @@
 package com.tngtech.jgiven.spock
 
 import com.tngtech.jgiven.annotation.As
+
+import com.tngtech.jgiven.spock.ScenarioSpec
+import com.tngtech.jgiven.spock.stages.Given
+import com.tngtech.jgiven.spock.stages.Then
+import com.tngtech.jgiven.spock.stages.When
+
 import spock.lang.Unroll
 
 @As('My shiny JGiven should')
