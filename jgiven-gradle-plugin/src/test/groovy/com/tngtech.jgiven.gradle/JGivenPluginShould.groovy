de98be01bf806212d0f352050b3be02819a565d8
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2021-04-13 07:24:43 +0200

    make all tests work nicely with groovy 3.
    The upgrade to gradle 7 makes gradle incompatible with spock-1.x, because that runs on groovy-2.5. Therefore an upgrade to spock-2 is necessary, which is based on Junit5.
    
     A main complication were the Temporary Folders, which have been replaced by a different mechanism in JUnit5. While simply reusing the Temporary Folder manually would have been possible, I
    
    opted to use spock's tempdir annotation order to avoid potential suffering from future incompatabilities. Also it turned out that many of the "TemporaryFolder"'s capabilities can be easily achived with java's File object.
    
    For the JUnit tests, a file wrapper object had to be introduced to keep the structure of the test similar to the way it was before.
    
    This is because JUnit5's @TempDir annotation only becomes active after the "postProcessTestInstance" phase, in which JGiven passes around the reference for the Scenario state. Using temp dir directly thus results in a null pointer exception
    
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>
    
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-gradle-plugin/src/test/groovy/com/tngtech.jgiven.gradle/JGivenPluginShould.groovy
+++ jgiven-gradle-plugin/src/test/groovy/com/tngtech.jgiven.gradle/JGivenPluginShould.groovy
@@ -1,23 +1,21 @@
 package com.tngtech.jgiven.gradle
 
-
 import com.google.common.io.Resources
 import org.gradle.testkit.runner.GradleRunner
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
+import spock.lang.TempDir
 
 import static org.gradle.testkit.runner.TaskOutcome.FROM_CACHE
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class JGivenPluginShould extends Specification {
 
-    @Rule
-    TemporaryFolder testProjectDir = new TemporaryFolder()
+    @TempDir
+    File testProjectDir
     File buildFile
 
     def setup() {
-        buildFile = testProjectDir.newFile('build.gradle')
+        buildFile = new File(testProjectDir,'build.gradle')
     }
 
     def "test is cacheable"() {
@@ -28,21 +26,21 @@ class JGivenPluginShould extends Specification {
             id 'com.tngtech.jgiven.gradle-plugin'
         }
         
-       repositories { jcenter() }
+       repositories { mavenCentral() }
        dependencies {
-         testImplementation 'com.tngtech.jgiven:jgiven-junit:1.0.0-RC4' 
-         testImplementation 'junit:junit:4.12'
+         testImplementation 'com.tngtech.jgiven:jgiven-junit:1.0.0' 
+         testImplementation 'junit:junit:4.13.1' 
         }
     """
 
-        testProjectDir.newFolder("src", "test", "java")
-        File scenario = testProjectDir.newFile("src/test/java/SimpleScenario.java")
+        new File(testProjectDir,"src/test/java").mkdirs()
+        File scenario = new File(testProjectDir,"src/test/java/SimpleScenario.java")
         scenario << Resources.toByteArray(Resources.getResource("SimpleScenario.java"))
 
         when:
         def result = GradleRunner.create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withArguments("--build-cache", "test", "jgivenTestReport", "-S")
+                .withProjectDir(testProjectDir)
+                .withArguments("--build-cache", "test", "jgivenTestReport", "-S", "--debug")
                 .withPluginClasspath()
                 .build()
 
@@ -51,10 +49,10 @@ class JGivenPluginShould extends Specification {
         result.task(":jgivenTestReport").outcome == SUCCESS
 
         when:
-        new File(testProjectDir.root, 'build').deleteDir()
+        new File(testProjectDir, 'build').deleteDir()
         result = GradleRunner.create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withArguments("--build-cache", "test", "jgivenTestReport", "-S")
+                .withProjectDir(testProjectDir)
+                .withArguments("--build-cache", "test", "jgivenTestReport", "-S", "--debug")
                 .withPluginClasspath()
                 .build()
 
