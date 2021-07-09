2f672d150a576d6111c7c40a0157b045673fbf9e
Andru Stefanescu <als209@cam.ac.uk>
2021-07-09 17:34:47 +0100

    Change project location for Timer and implement timer using Google's Stopwatch
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

---
+++ jgiven-core/src/main/java/com/tngtech/jgiven/impl/Timer.java
@@ -0,0 +1,22 @@
+package com.tngtech.jgiven.impl;
+
+import com.google.common.base.Stopwatch;
+import java.util.concurrent.TimeUnit;
+
+public class Timer {
+  Stopwatch timer;
+
+  public Timer() {
+    this.timer = Stopwatch.createStarted();
+  }
+
+  public void reset() {
+   this.timer.reset();
+   assert (this.timer.isRunning());
+  }
+
+  public long stop() {
+    timer.stop();
+    return timer.elapsed(TimeUnit.MILLISECONDS);
+  }
+}
