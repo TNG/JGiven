ebe8402f70718e97a30c5e310322ccfda2ddf403
Andru Stefanescu <als209@cam.ac.uk>
2021-07-22 14:22:19 +0100

    Reformat code
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

--- jgiven-core/src/main/java/com/tngtech/jgiven/impl/ByteBuddyStageClassCreator.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/impl/ByteBuddyStageClassCreator.java
@@ -19,50 +19,51 @@ public class ByteBuddyStageClassCreator implements StageClassCreator {
     public static final String STAGE_NAME_FIELD_NAME = "__jgiven_stageName";
     public static final String SETTER_NAME = "__jgiven_setStepInterceptor";
 
-    public interface StepInterceptorGetterSetter {
-        Object getValue();
-        void setValue(Object value);
-    }
-
-    public static class StepInterceptorSetter {
-        public void interceptSetter(StepInterceptor interceptor,
-                                    @FieldProxy( INTERCEPTOR_FIELD_NAME ) StepInterceptorGetterSetter stepInterceptorSetter) {
-            stepInterceptorSetter.setValue(interceptor);
-        }
-    }
-
-    public <T> Class<? extends T> createStageClass( Class<T> stageClass ) {
+    public <T> Class<? extends T> createStageClass(Class<T> stageClass) {
         return new ByteBuddy()
-            .subclass( stageClass, ConstructorStrategy.Default.IMITATE_SUPER_CLASS_OPENING )
-            .implement( StageInterceptorInternal.class )
-            .defineField( INTERCEPTOR_FIELD_NAME, StepInterceptor.class )
-            .defineField(STAGE_NAME_FIELD_NAME, String.class)
-            .method( named(SETTER_NAME) )
+                .subclass(stageClass, ConstructorStrategy.Default.IMITATE_SUPER_CLASS_OPENING)
+                .implement(StageInterceptorInternal.class)
+                .defineField(INTERCEPTOR_FIELD_NAME, StepInterceptor.class)
+                .defineField(STAGE_NAME_FIELD_NAME, String.class)
+                .method(named(SETTER_NAME))
                 .intercept(
-                    MethodDelegation.withDefaultConfiguration()
-                        .withBinders( FieldProxy.Binder.install(
-                                StepInterceptorGetterSetter.class ))
-                .to(new StepInterceptorSetter() ))
-            .method( not( named( SETTER_NAME )
-                    .or(ElementMatchers.isDeclaredBy(Object.class))))
-            .intercept(
-                    MethodDelegation.withDefaultConfiguration()
-                    .withBinders(FieldProxy.Binder.install(
-                            StepInterceptorGetterSetter.class ))
-                .to( new ByteBuddyMethodInterceptor() ))
-            .make()
-            .load( getClassLoader(stageClass),
-                getClassLoadingStrategy( stageClass ) )
-            .getLoaded();
+                        MethodDelegation.withDefaultConfiguration()
+                                .withBinders(FieldProxy.Binder.install(
+                                        StepInterceptorGetterSetter.class))
+                                .to(new StepInterceptorSetter()))
+                .method(not(named(SETTER_NAME)
+                        .or(ElementMatchers.isDeclaredBy(Object.class))))
+                .intercept(
+                        MethodDelegation.withDefaultConfiguration()
+                                .withBinders(FieldProxy.Binder.install(
+                                        StepInterceptorGetterSetter.class))
+                                .to(new ByteBuddyMethodInterceptor()))
+                .make()
+                .load(getClassLoader(stageClass),
+                        getClassLoadingStrategy(stageClass))
+                .getLoaded();
     }
 
-    protected ClassLoadingStrategy getClassLoadingStrategy( Class<?> stageClass ) {
+    protected ClassLoadingStrategy getClassLoadingStrategy(Class<?> stageClass) {
         return getClassLoader(stageClass) == null
                 ? ClassLoadingStrategy.Default.WRAPPER
                 : ClassLoadingStrategy.Default.INJECTION;
     }
 
-    protected ClassLoader getClassLoader( Class<?> stageClass ) {
+    protected ClassLoader getClassLoader(Class<?> stageClass) {
         return stageClass.getClassLoader();
+    }
+
+    public interface StepInterceptorGetterSetter {
+        Object getValue();
+
+        void setValue(Object value);
+    }
+
+    public static class StepInterceptorSetter {
+        public void interceptSetter(StepInterceptor interceptor,
+                                    @FieldProxy(INTERCEPTOR_FIELD_NAME) StepInterceptorGetterSetter stepInterceptorSetter) {
+            stepInterceptorSetter.setValue(interceptor);
+        }
     }
 }
