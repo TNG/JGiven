c5c5ccece52b94a30efe7ba52386132a37abe96f
Andru Stefanescu <als209@cam.ac.uk>
2021-07-28 15:58:46 +0100

    Reformat code
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

--- jgiven-tests/src/test/java/com/tngtech/jgiven/report/html5/Html5AppStage.java
+++ jgiven-tests/src/test/java/com/tngtech/jgiven/report/html5/Html5AppStage.java
@@ -2,16 +2,14 @@ package com.tngtech.jgiven.report.html5;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.File;
-import java.util.List;
-
-import org.openqa.selenium.*;
-
 import com.tngtech.jgiven.CurrentStep;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.attachment.Attachment;
 import com.tngtech.jgiven.attachment.MediaType;
+import java.io.File;
+import java.util.List;
+import org.openqa.selenium.*;
 
 public class Html5AppStage<SELF extends Html5AppStage<?>> extends Stage<SELF> {
     @ExpectedScenarioState
@@ -24,15 +22,15 @@ public class Html5AppStage<SELF extends Html5AppStage<?>> extends Stage<SELF> {
     protected File targetReportDir;
 
     protected void takeScreenshot() {
-        String base64 = ( (TakesScreenshot) webDriver ).getScreenshotAs( OutputType.BASE64 );
-        currentStep.addAttachment( Attachment.fromBase64( base64, MediaType.PNG ).withTitle( "Screenshot" ) );
+        String base64 = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BASE64);
+        currentStep.addAttachment(Attachment.fromBase64(base64, MediaType.PNG).withTitle("Screenshot"));
     }
 
-    protected WebElement findTagWithName( String tagName ) {
+    protected WebElement findTagWithName(String tagName) {
         List<WebElement> links = webDriver.findElements(
-            By.xpath( String.format( "//a/span[contains(@class,'tag') and contains(text(), '%s')]/..", tagName ) ) );
-        assertThat( links ).isNotEmpty();
-        return links.get( 0 );
+                By.xpath(String.format("//a/span[contains(@class,'tag') and contains(text(), '%s')]/..", tagName)));
+        assertThat(links).isNotEmpty();
+        return links.get(0);
     }
 
 }
