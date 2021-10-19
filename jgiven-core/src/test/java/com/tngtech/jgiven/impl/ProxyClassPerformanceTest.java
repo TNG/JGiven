6f5f0ebf69ff277b3cda4eea1d8266e397eafc3d
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2021-10-19 07:39:29 +0200

    Improve expressiveness of proxy class memory usage test
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-core/src/test/java/com/tngtech/jgiven/impl/ProxyClassPerformanceTest.java
+++ jgiven-core/src/test/java/com/tngtech/jgiven/impl/ProxyClassPerformanceTest.java
@@ -1,36 +1,41 @@
 package com.tngtech.jgiven.impl;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
 
 public class ProxyClassPerformanceTest {
 
     @Test
-    public void test_creation_of_proxy_classes(){
-        Set<Long> results = new HashSet<>();
-        for( int i = 0; i < 1000; i++ ) {
+    public void test_creation_of_proxy_classes() {
+        Set<Long> megabytesOfMemoryUsedInCycle = new HashSet<>();
+        for (int i = 0; i < 1000; i++) {
             ScenarioBase scenario = new ScenarioBase();
-            TestStage testStage = scenario.addStage( TestStage.class );
+            TestStage testStage = scenario.addStage(TestStage.class);
             testStage.something();
-            if( i % 100 == 0 ) {
+            if (i % 100 == 0) {
                 System.gc();
-                long usedMemory = ( Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() ) / ( 1024 * 1024 );
-                System.out.println( "Used memory: " + usedMemory );
-                results.add( usedMemory );
+                long usedMemory = calculateMemoryUsageRoundedDownToMegabytes();
+                System.out.println("Used memory: " + usedMemory);
+                megabytesOfMemoryUsedInCycle.add(usedMemory);
             }
         }
 
-        assertThat( results )
-                .describedAs( "Only should contains 1 or 2 items, as first iteration might use more memory might contain 2 items" )
-                .hasSizeBetween( 1, 2 );
+        assertThat(megabytesOfMemoryUsedInCycle)
+            .describedAs("Set should only contain 1 item, "
+                    + "but the  first iteration might use more memory, "
+                    + "so the set might contain 2 items")
+            .hasSizeBetween(1, 2);
+    }
+
+    private long calculateMemoryUsageRoundedDownToMegabytes() {
+        return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
     }
 
     public static class TestStage {
-        public void something(){
+        public void something() {
         }
     }
 }
