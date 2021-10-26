60f2a8821fbe4c2c650815f4dc031be16da0dd3a
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2021-10-26 08:09:25 +0200

    Alter Memory consumption test
    according to Issue-294 there was a memory leak at the beginning of using ByteBuddy. The current test checked that the memory consumption always stayed the same, using the global JVM memory to calculate usage.
    However, varying values of consumption, though not in an ever increasing fashion, were observed in our CI pipeline, thus failing this test.
    
    We therefore change the assertion to mirror its assumed original goal more closely by just requiring that there is no overall increase in consumption over the runtime of the test.
    
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-core/src/test/java/com/tngtech/jgiven/impl/ProxyClassPerformanceTest.java
+++ jgiven-core/src/test/java/com/tngtech/jgiven/impl/ProxyClassPerformanceTest.java
@@ -2,39 +2,61 @@ package com.tngtech.jgiven.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
+/**
+ * test that the continued creation of proxy classes does not
+ * constantly consume memory.
+ */
 public class ProxyClassPerformanceTest {
+
+    private static final int NUMBER_OF_RUNS = 10000;
+    private static final int PROBE_INTERVAL = 100;
 
     @Test
     public void test_creation_of_proxy_classes() {
-        Set<Long> megabytesOfMemoryUsedInCycle = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
+        List<Long> memoryUsageRecordInMebibytes = new ArrayList<>(NUMBER_OF_RUNS / PROBE_INTERVAL);
+        for (int i = 0; i < NUMBER_OF_RUNS; i++) {
             ScenarioBase scenario = new ScenarioBase();
             TestStage testStage = scenario.addStage(TestStage.class);
             testStage.something();
-            if (i % 100 == 0) {
+            if (i % PROBE_INTERVAL == 0) {
                 System.gc();
-                long usedMemory = calculateMemoryUsageRoundedDownToMegabytes();
+                long usedMemory = calculateMemoryUsageRoundedDownToMebibytes();
                 System.out.println("Used memory: " + usedMemory);
-                megabytesOfMemoryUsedInCycle.add(usedMemory);
+                memoryUsageRecordInMebibytes.add(usedMemory);
             }
         }
-
-        assertThat(megabytesOfMemoryUsedInCycle)
-            .describedAs("Set should only contain 1 item, "
-                    + "but the  first iteration might use more memory, "
-                    + "so the set might contain 2 items")
-            .hasSizeBetween(1, 2);
+        List<Long> growth = calculateChangeInMemoryConsumption(memoryUsageRecordInMebibytes);
+        double averageConsumptionChange = average(growth);
+        assertThat(averageConsumptionChange)
+            .as("There is no net increase of memory consumption "
+            + "for the continued creation and discarding of proxy classes.")
+            .isLessThanOrEqualTo(0);
     }
 
-    private long calculateMemoryUsageRoundedDownToMegabytes() {
+    private List<Long> calculateChangeInMemoryConsumption(List<Long> record) {
+        List<Long> growth = new ArrayList<>(record.size() - 1);
+        for (int i = 1; i < record.size(); i++) {
+            growth.add(record.get(i) - record.get(i - 1));
+        }
+        return growth;
+    }
+
+    private double average(List<Long> data) {
+        return data.stream()
+            .mapToDouble(Double::valueOf)
+            .reduce((currentAverage, dataPoint) -> currentAverage + dataPoint / data.size())
+            .orElse(0.0);
+    }
+
+    private long calculateMemoryUsageRoundedDownToMebibytes() {
         return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
     }
 
-    public static class TestStage {
+    static class TestStage {
         public void something() {
         }
     }
