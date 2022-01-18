ba9394fc1ac62a625e4c1ece5889587add17ab69
Philippon Sebastien <sphilippon@oui.sncf>
2022-01-18 11:21:17 +0100

    add tests to the JUnit 5 tag configuration override feature
    Signed-off-by: Philippon Sebastien <sphilippon@oui.sncf>

--- jgiven-junit5/src/test/java/com/tngtech/jgiven/junit5/test/JUnit5ExtensionTest.java
+++ jgiven-junit5/src/test/java/com/tngtech/jgiven/junit5/test/JUnit5ExtensionTest.java
@@ -1,11 +1,13 @@
 package com.tngtech.jgiven.junit5.test;
 
 import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.config.ConfigurationUtil;
+import com.tngtech.jgiven.config.TagConfiguration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ScenarioStage;
 import com.tngtech.jgiven.junit5.JGivenExtension;
 
@@ -34,4 +36,14 @@ public class JUnit5ExtensionTest {
         whenStage.some_failing_step();
     }
 
+    @Test
+    @Tag( "test-tag" )
+    public void JUnit5_tags_are_converted_to_JGiven_tags_using_default_configuration() {
+        TagConfiguration configuration = ConfigurationUtil.getConfiguration( JUnit5ExtensionTest.class )
+                .getTagConfiguration( Tag.class );
+
+        Assertions.assertNotNull( configuration );
+        Assertions.assertEquals( "JUnit 5 Tag", configuration.getDescription() );
+        Assertions.assertEquals( "orange", configuration.getColor() );
+    }
 }
