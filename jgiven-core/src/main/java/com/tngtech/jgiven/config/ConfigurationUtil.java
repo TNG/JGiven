c108e687b0d85ae937b1669fb269f63376078353
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2022-01-19 08:21:53 +0100

    Introduce optional into the configuration util to simplify the code
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-core/src/main/java/com/tngtech/jgiven/config/ConfigurationUtil.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/config/ConfigurationUtil.java
@@ -1,39 +1,39 @@
 package com.tngtech.jgiven.config;
 
-import java.util.concurrent.ExecutionException;
-
-import com.google.common.base.Throwables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.tngtech.jgiven.annotation.JGivenConfiguration;
 import com.tngtech.jgiven.impl.util.ReflectionUtil;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
 
 public class ConfigurationUtil {
 
-    private static final LoadingCache<Class<?>, AbstractJGivenConfiguration> configurations = CacheBuilder.newBuilder().build(
-        new CacheLoader<Class<?>, AbstractJGivenConfiguration>() {
-            @Override
-            public AbstractJGivenConfiguration load( Class<?> key ) throws Exception {
-                AbstractJGivenConfiguration result = (AbstractJGivenConfiguration) ReflectionUtil.newInstance( key );
-                result.configure();
-                return result;
-            }
-        } );
+    private static final LoadingCache<Class<?>, AbstractJGivenConfiguration> configurations =
+        CacheBuilder.newBuilder().build(
+            new CacheLoader<Class<?>, AbstractJGivenConfiguration>() {
+                @Override
+                public AbstractJGivenConfiguration load(Class<?> key) {
+                    AbstractJGivenConfiguration result = (AbstractJGivenConfiguration) ReflectionUtil.newInstance(key);
+                    result.configure();
+                    return result;
+                }
+            });
 
-    public static AbstractJGivenConfiguration getConfiguration( Class<? extends Object> testClass ) {
-        JGivenConfiguration annotation = testClass.getAnnotation( JGivenConfiguration.class );
-        Class<?> configuration;
-        if( annotation == null ) {
-            configuration = DefaultConfiguration.class;
-        } else {
-            configuration = annotation.value();
-        }
+    @SuppressWarnings({"unchecked"})
+    public static <A extends AbstractJGivenConfiguration> AbstractJGivenConfiguration getConfiguration(
+        Class<?> testClass) {
+        Class<? extends AbstractJGivenConfiguration> configuration = Optional.ofNullable(testClass)
+            .map(content -> content.getAnnotation(JGivenConfiguration.class))
+            .map(content -> (Class<A>) content.value())
+            .orElse((Class<A>) DefaultConfiguration.class);
 
         try {
-            return configurations.get( configuration );
-        } catch( ExecutionException e ) {
-            throw Throwables.propagate( e.getCause() );
+            return configurations.get(configuration);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e.getCause());
         }
     }
 }
