32949053f13c77112f2cdc217c482f276406fe5b
l-1squared <30831153+l-1squared@users.noreply.github.com>
2022-03-29 07:56:25 +0200

    Update code to new selenium interface
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-tests/src/test/java/com/tngtech/jgiven/report/html5/Html5AppStage.java
+++ jgiven-tests/src/test/java/com/tngtech/jgiven/report/html5/Html5AppStage.java
@@ -8,13 +8,14 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.attachment.Attachment;
 import com.tngtech.jgiven.attachment.MediaType;
 import java.io.File;
+import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Html5AppStage<SELF extends Html5AppStage<?>> extends Stage<SELF> {
-    private static final int WEBDRIVER_FIND_TIMEOUT_SECONDS = 120;
+    private static final Duration WEBDRIVER_FIND_TIMEOUT = Duration.ofMinutes(2);
     @ExpectedScenarioState
     protected CurrentStep currentStep;
 
@@ -30,7 +31,7 @@ public class Html5AppStage<SELF extends Html5AppStage<?>> extends Stage<SELF> {
     }
 
     protected WebElement findTagWithName(String tagName) {
-        WebDriverWait timeoutSetter = new WebDriverWait(webDriver, WEBDRIVER_FIND_TIMEOUT_SECONDS);
+        WebDriverWait timeoutSetter = new WebDriverWait(webDriver, WEBDRIVER_FIND_TIMEOUT);
 
         By elementLocator = By.xpath(String.format("//a/span[contains(@class,'tag') and contains(text(), '%s')]/..",
                 tagName));
