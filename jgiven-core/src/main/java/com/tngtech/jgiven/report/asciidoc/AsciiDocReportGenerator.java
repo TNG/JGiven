01ece27533974bb4fc5a0e6dd74ebd7a57bbd99b
Johannes Thorn <johthor@users.noreply.github.com>
2022-12-25 15:49:36 +0100

    Restructure report generation
    Signed-off-by: Johannes Thorn <johthor@users.noreply.github.com>

--- jgiven-core/src/main/java/com/tngtech/jgiven/report/asciidoc/AsciiDocReportGenerator.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/report/asciidoc/AsciiDocReportGenerator.java
@@ -12,35 +12,43 @@ import com.tngtech.jgiven.report.*;
 import com.tngtech.jgiven.report.model.ReportModel;
 import com.tngtech.jgiven.report.model.ReportModelFile;
 import com.tngtech.jgiven.report.model.ReportStatistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AsciiDocReportGenerator extends AbstractReportGenerator {
 
-    private final List<String> allFiles = Lists.newArrayList();
+    private static final Logger log = LoggerFactory.getLogger( AsciiDocReportGenerator.class );
+
+    private final List<String> features = Lists.newArrayList();
 
     public AbstractReportConfig createReportConfig(String... args) {
         return new AsciiDocReportConfig(args);
     }
 
     public void generate() {
+        File targetDir = config.getTargetDir();
+        if (!targetDir.exists() && !targetDir.mkdirs()) {
+            log.error("Could not ensure target directory exists {}", targetDir);
+            return;
+        }
+
         for (ReportModelFile reportModelFile : completeReportModel.getAllReportModels()) {
             writeReportModelToFile(reportModelFile.model, reportModelFile.file);
         }
+
         generateIndexFile();
     }
 
     private void writeReportModelToFile(ReportModel model, File file) {
-        String targetFileName = Files.getNameWithoutExtension(file.getName()) + ".asciidoc";
+        String featureFileName = Files.getNameWithoutExtension(file.getName()) + ".asciidoc";
 
-        allFiles.add(targetFileName);
-        if (!config.getTargetDir().exists()) {
-            config.getTargetDir().mkdirs();
-        }
-        File targetFile = new File(config.getTargetDir(), targetFileName);
+        features.add(featureFileName);
+        File targetFile = new File(config.getTargetDir(), featureFileName);
         PrintWriter printWriter = PrintWriterUtil.getPrintWriter(targetFile);
 
-
         try {
-            AsciiDocWriter visitor = new AsciiDocWriter(new AsciiDocReportModelHandler(printWriter));
+            AsciiDocReportModelHandler handler = new AsciiDocReportModelHandler(printWriter);
+            AsciiDocReportModelVisitor visitor = new AsciiDocReportModelVisitor(handler);
             model.accept(visitor);
         } finally {
             ResourceUtil.close(printWriter);
@@ -48,63 +56,64 @@ public class AsciiDocReportGenerator extends AbstractReportGenerator {
     }
 
     private void generateIndexFile() {
-        PrintWriter printWriter = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "index.asciidoc"));
-        try {
-            printWriter.println("= JGiven Documentation");
-            printWriter.println(":toc: left");
-            printWriter.println(":toclevels: 5");
-            printWriter.println("");
-            printWriter.println("include::totalStatistics.asciidoc[]");
-            printWriter.println("");
-            printWriter.println("== All Scenarios");
-            printWriter.println("");
-
-            printWriter.println("include::allClasses.asciidoc[]");
-        } finally {
-            ResourceUtil.close(printWriter);
+        try(PrintWriter printWriter = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "index.asciidoc"))) {
+            convertIndex(printWriter);
         }
 
-        writeTotalStatistics();
+        try (PrintWriter writer = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "totalStatistics.asciidoc"))) {
+            generateStatistics(writer);
+        }
 
-        printWriter = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "allClasses.asciidoc"));
-        try {
-            for (String fileName : allFiles) {
-                printWriter.println("include::" + fileName + "[]\n");
-            }
-        } finally {
-            ResourceUtil.close(printWriter);
+        try (PrintWriter printWriter = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "allClasses.asciidoc"))) {
+            generateIncludes(printWriter);
         }
     }
 
-    private void writeTotalStatistics() {
-        try (PrintWriter writer = PrintWriterUtil.getPrintWriter(new File(config.getTargetDir(), "totalStatistics.asciidoc"))) {
+    private void generateStatistics(PrintWriter writer) {
+        writer.println();
+        writer.println(".Total Statistics");
+        writer.println("[options=\"header,footer\"]");
+        writer.println("|===");
 
-            writer.println();
-            writer.println(".Total Statistics");
-            writer.println("[options=\"header,footer\"]");
-            writer.println("|===");
-
-            writer.print("| feature ");
-            writer.print("| total classes ");
-            writer.print("| successful scenarios ");
-            writer.print("| failed scenarios ");
-            writer.print("| pending scenarios ");
-            writer.print("| total scenarios ");
-            writer.print("| failed cases ");
-            writer.print("| total cases ");
-            writer.print("| total steps ");
-            writer.println("| duration");
+        writer.print("| feature ");
+        writer.print("| total classes ");
+        writer.print("| successful scenarios ");
+        writer.print("| failed scenarios ");
+        writer.print("| pending scenarios ");
+        writer.print("| total scenarios ");
+        writer.print("| failed cases ");
+        writer.print("| total cases ");
+        writer.print("| total steps ");
+        writer.println("| duration");
 
 
-            completeReportModel.getAllReportModels().forEach(reportModelFile -> {
-                ReportStatistics statistics = completeReportModel.getStatistics(reportModelFile);
-                extracted(writer, reportModelFile.model.getName(), statistics);
-            });
+        completeReportModel.getAllReportModels().forEach(reportModelFile -> {
+            ReportStatistics statistics = completeReportModel.getStatistics(reportModelFile);
+            extracted(writer, reportModelFile.model.getName(), statistics);
+        });
 
-            extracted(writer, "sum", completeReportModel.getTotalStatistics());
+        extracted(writer, "sum", completeReportModel.getTotalStatistics());
 
-            writer.println("|===");
+        writer.println("|===");
+    }
+
+    private void generateIncludes(PrintWriter printWriter) {
+        for (String fileName : features) {
+            printWriter.println("include::" + fileName + "[]\n");
         }
+    }
+
+    private static void convertIndex(PrintWriter printWriter) {
+        printWriter.println("= JGiven Documentation");
+        printWriter.println(":toc: left");
+        printWriter.println(":toclevels: 5");
+        printWriter.println("");
+        printWriter.println("include::totalStatistics.asciidoc[]");
+        printWriter.println("");
+        printWriter.println("== All Scenarios");
+        printWriter.println("");
+
+        printWriter.println("include::allClasses.asciidoc[]");
     }
 
     private static void extracted(PrintWriter writer, String name, ReportStatistics statistics) {
