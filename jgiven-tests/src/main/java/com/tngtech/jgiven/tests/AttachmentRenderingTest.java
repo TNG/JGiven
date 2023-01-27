b8c3fc6920f5def92ee85d283b02bb6fff8009bf
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-01-27 11:38:39 +0100

    Issue-1131: wrote test to verify that the json is produced correctly
    TODO: check if the case is already covered by lower level tests.
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-tests/src/main/java/com/tngtech/jgiven/tests/AttachmentRenderingTest.java
+++ jgiven-tests/src/main/java/com/tngtech/jgiven/tests/AttachmentRenderingTest.java
@@ -1,8 +1,9 @@
 package com.tngtech.jgiven.tests;
 
+import com.tngtech.jgiven.CurrentStep;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.NestedSteps;
-import com.tngtech.jgiven.annotation.ScenarioStage;
+import com.tngtech.jgiven.annotation.ScenarioState;
 import com.tngtech.jgiven.attachment.Attachment;
 import com.tngtech.jgiven.attachment.MediaType;
 import com.tngtech.jgiven.testng.ScenarioTestListener;
@@ -10,14 +11,10 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testng.annotations.Listeners;
 
-import static com.tngtech.jgiven.tests.AttachmentRenderingTest.*;
+import static com.tngtech.jgiven.tests.AttachmentRenderingTest.AttachmentRenderingStages;
 @Listeners( ScenarioTestListener.class )
 @ExtendWith(JGivenReportExtractingExtension.class)
 public class AttachmentRenderingTest extends ScenarioTestForTesting<AttachmentRenderingStages, AttachmentRenderingStages, AttachmentRenderingStages> {
-
-    public AttachmentRenderingTest() {
-        System.out.println("nasiges");
-    }
 
     @Test
     @org.junit.jupiter.api.Test
@@ -30,8 +27,12 @@ public class AttachmentRenderingTest extends ScenarioTestForTesting<AttachmentRe
 
     @SuppressWarnings("UnusedReturnValue")
     static class AttachmentRenderingStages extends Stage<AttachmentRenderingStages> {
+
+        @ScenarioState
+        CurrentStep currentStep;
         AttachmentRenderingStages a_step_with_attachment() {
-            Attachment.fromText("a_step_with_attachment", MediaType.PLAIN_TEXT_UTF_8);
+            Attachment attachment = Attachment.fromText("a step with attachment", MediaType.PLAIN_TEXT_UTF_8);
+            currentStep.addAttachment(attachment);
             return self();
         }
 
@@ -43,16 +44,20 @@ public class AttachmentRenderingTest extends ScenarioTestForTesting<AttachmentRe
         }
 
         AttachmentRenderingStages a_final_step() {
+            Attachment attachment = Attachment.fromText("a final step", MediaType.PLAIN_TEXT_UTF_8);
+            currentStep.addAttachment(attachment);
             return self();
         }
 
         AttachmentRenderingStages first_nested_step_with_attachment() {
-            Attachment.fromText("first_nested_step_with_attachment", MediaType.PLAIN_TEXT_UTF_8);
+            Attachment attachment = Attachment.fromText("first nested step with attachment", MediaType.PLAIN_TEXT_UTF_8);
+            currentStep.addAttachment(attachment);
             return self();
         }
 
         AttachmentRenderingStages second_nested_step_with_attachment() {
-            Attachment.fromText("second_nested_step_with_attachment", MediaType.PLAIN_TEXT_UTF_8);
+            Attachment attachment = Attachment.fromText("second nested step with attachment", MediaType.PLAIN_TEXT_UTF_8);
+            currentStep.addAttachment(attachment);
             return self();
         }
     }
--- jgiven-tests/src/test/java/com/tngtech/jgiven/report/json/AttachmentRenderingTest.java
+++ jgiven-tests/src/test/java/com/tngtech/jgiven/report/json/AttachmentRenderingTest.java
@@ -1,43 +1,39 @@
 package com.tngtech.jgiven.report.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.junit.SimpleScenarioTest;
-import com.tngtech.jgiven.report.html5.Html5ReportConfig;
-import com.tngtech.jgiven.report.html5.Html5ReportGenerator;
+import com.tngtech.jgiven.report.model.AttachmentModel;
 import com.tngtech.jgiven.report.model.ReportModel;
+import com.tngtech.jgiven.report.model.ScenarioModel;
+import com.tngtech.jgiven.report.model.StepModel;
 import com.tngtech.jgiven.tags.Issue;
 import com.tngtech.jgiven.testframework.TestExecutionResult;
 import com.tngtech.jgiven.testframework.TestExecutor;
 import com.tngtech.jgiven.testframework.TestFramework;
 import com.tngtech.jgiven.tests.TestScenarioRepository;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Request;
-import org.openqa.selenium.json.Json;
 
-import static com.tngtech.jgiven.report.json.AttachmentRenderingTest.*;
+import java.util.List;
+
+import static com.tngtech.jgiven.report.json.AttachmentRenderingTest.TestExecutionStages;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Issue("1131")
 public class AttachmentRenderingTest extends SimpleScenarioTest<TestExecutionStages> {
 
     @Test
     public void testJsonIsRenderedCorrectly() {
-        given().a_test_that_produces_many_attachments();
-        when().the_test_gets_executed().and().the_report_gets_rendered();
-        then().do_something();
+        given().a_test_where_every_terminal_step_has_an_attachment_of_its_name();
+        when().the_test_gets_executed();
+        then().for_each_terminal_step_the_report_has_an_attachment_of_the_method_name();
     }
 
     @SuppressWarnings("UnusedReturnValue")
     static class TestExecutionStages extends Stage<TestExecutionStages> {
         private TestScenarioRepository.TestScenario testScenario;
         private ReportModel reportModel;
-        private JsonObject reportJson;
 
-        TestExecutionStages a_test_that_produces_many_attachments(){
+        TestExecutionStages a_test_where_every_terminal_step_has_an_attachment_of_its_name(){
             testScenario = TestScenarioRepository.attachmentRenderingTest();
             return self();
         }
@@ -49,15 +45,24 @@ public class AttachmentRenderingTest extends SimpleScenarioTest<TestExecutionSta
             return self();
         }
 
-        TestExecutionStages the_report_gets_rendered(){
-            reportJson = JsonParser.parseString(new ScenarioJsonWriter(reportModel).toString()).getAsJsonObject();
+        TestExecutionStages for_each_terminal_step_the_report_has_an_attachment_of_the_method_name(){
+            assertThat(reportModel.getScenarios())
+                    .hasSize(1)
+                    .extracting(ScenarioModel::getScenarioCases)
+                    .extracting(List::size)
+                    .containsExactly(1);
+            reportModel.getLastScenarioModel().getCase(0).getSteps().forEach(this::assertStep);
             return self();
         }
 
-        TestExecutionStages do_something(){
-            //TODO: assertions
-           System.out.println("tarlalsl");
-           return self();
+        private void assertStep(StepModel step){
+            if(!step.getNestedSteps().isEmpty()) {
+                step.getNestedSteps().forEach(this::assertStep);
+            } else {
+                assertThat(step.getAttachments())
+                        .extracting(AttachmentModel::getValue)
+                        .containsExactly(step.getName());
+            }
         }
     }
 }
