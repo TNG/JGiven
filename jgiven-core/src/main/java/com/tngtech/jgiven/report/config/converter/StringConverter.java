67c09753dc63c610a7cae42a16b175012e94d371
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-03-03 18:05:41 +0100

    remove obsolete javadoc from StringConverter
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-core/src/main/java/com/tngtech/jgiven/report/config/converter/StringConverter.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/report/config/converter/StringConverter.java
@@ -4,7 +4,7 @@ import com.tngtech.jgiven.report.AbstractReportGenerator;
 
 /**
  * Interface to create converter function to specify the to map the incoming command line argument to the object
- * See {@link AbstractReportGenerator#populateConfigOptions()} for example definitions of {@link com.tngtech.jgiven.report.config.ConfigOption}
+ * See {@link AbstractReportGenerator#createConfigOptions()} for example definitions of {@link com.tngtech.jgiven.report.config.ConfigOption}
  *
  * If the conversion is not possible, return null. The {@link com.tngtech.jgiven.report.config.ConfigOptionParser} terminates gracefully
  */
