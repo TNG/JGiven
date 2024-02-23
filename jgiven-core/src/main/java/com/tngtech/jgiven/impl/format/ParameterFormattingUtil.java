95905737359405c6947a4ce6839609982c6ff33c
l-1squared <30831153+l-1squared@users.noreply.github.com>
2024-02-23 08:27:57 +0100

    remove deprecated calls to newInstance
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-core/src/main/java/com/tngtech/jgiven/impl/format/ParameterFormattingUtil.java
+++ jgiven-core/src/main/java/com/tngtech/jgiven/impl/format/ParameterFormattingUtil.java
@@ -70,7 +70,7 @@ public class ParameterFormattingUtil {
                 } else if( annotation instanceof AnnotationFormat ) {
                     AnnotationFormat arg = (AnnotationFormat) annotation;
                     foundFormatting.add( new StepFormatter.ArgumentFormatting(
-                        new StepFormatter.AnnotationBasedFormatter( arg.value().newInstance(), originalAnnotation ) ) );
+                        new StepFormatter.AnnotationBasedFormatter( arg.value().getConstructor().newInstance(), originalAnnotation ) ) );
                 } else {
                     Class<? extends Annotation> annotationType = annotation.annotationType();
                     if( !visitedTypes.contains( annotationType ) ) {
