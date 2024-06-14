81ae7efbe51bb578404158fedcd5964eaffdfaaf
l-1squared <30831153+l-1squared@users.noreply.github.com>
2024-06-14 12:46:30 +0200

    fix(Issue-1625): Refactor assumptions tests
    TODO:
    * TestNg and JUnit completely ignore all steps taken when an assumption fails, which is WRONG.
    
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-tests/src/main/java/com/tngtech/jgiven/tests/assumptions/JUnitAssumptionTestScenarios.java
+++ jgiven-tests/src/main/java/com/tngtech/jgiven/tests/assumptions/JUnitAssumptionTestScenarios.java
@@ -1,14 +1,29 @@
 package com.tngtech.jgiven.tests.assumptions;
 
+import com.tngtech.jgiven.annotation.ScenarioStage;
+import com.tngtech.jgiven.impl.Scenario;
+import com.tngtech.jgiven.junit.JGivenClassRule;
+import com.tngtech.jgiven.junit.JGivenMethodRule;
 import com.tngtech.jgiven.tests.GivenTestStage;
 import com.tngtech.jgiven.tests.ScenarioTestForTesting;
 import com.tngtech.jgiven.tests.ThenTestStage;
 import com.tngtech.jgiven.tests.WhenTestStage;
 import org.junit.Assume;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 @SuppressWarnings("NewClassNamingConvention")
-public class JUnitAssumptionTestScenarios extends ScenarioTestForTesting<GivenTestStage, WhenTestStage, ThenTestStage> {
+public class JUnitAssumptionTestScenarios {
+
+    @ClassRule
+    public static final JGivenClassRule writerRule = new JGivenClassRule();
+
+    @Rule
+    public final JGivenMethodRule scenarioRule = new JGivenMethodRule();
+
+    @ScenarioStage
+    GivenTestStage givenTestStage;
 
     @SuppressWarnings("DataFlowIssue")
     @Test
@@ -18,12 +33,12 @@ public class JUnitAssumptionTestScenarios extends ScenarioTestForTesting<GivenTe
 
     @Test
     public void test_with_failing_assumption_in_stage() {
-        given().a_failed_junit_assumption();
+        givenTestStage.given().a_failed_junit_assumption();
     }
 
     @Test
     public void test_with_failing_assumption_in_second_stage(){
-        given().nothing()
+        givenTestStage.given().nothing()
                 .a_failed_junit_assumption();
     }
 }
