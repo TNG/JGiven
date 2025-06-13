49185484914c1101a934a8f165ba10a586e7c5eb
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-06-13 10:27:51 +0200

    fix(Dependency Updates): JUnit 5.13 can retrieve parameter arguments
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-junit5/src/main/java/com/tngtech/jgiven/junit5/ArgumentReflectionUtil.java
+++ jgiven-junit5/src/main/java/com/tngtech/jgiven/junit5/ArgumentReflectionUtil.java
@@ -1,18 +1,19 @@
 package com.tngtech.jgiven.junit5;
 
+import com.tngtech.jgiven.impl.util.ParameterNameUtil;
+import com.tngtech.jgiven.impl.util.ReflectionUtil;
+import com.tngtech.jgiven.report.model.NamedArgument;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.support.ParameterInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.provider.Arguments;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.tngtech.jgiven.impl.util.ParameterNameUtil;
-import com.tngtech.jgiven.impl.util.ReflectionUtil;
-import com.tngtech.jgiven.report.model.NamedArgument;
+import java.util.function.Function;
 
 class ArgumentReflectionUtil {
     private static final Logger log = LoggerFactory.getLogger(ArgumentReflectionUtil.class);
@@ -24,37 +25,66 @@ class ArgumentReflectionUtil {
     static final String ERROR = "Not able to access field containing test method arguments. " +
             "Probably the internal representation has changed. Consider writing a bug report.";
 
+    static final List<Function<ExtensionContext, List<NamedArgument>>> argumentRetrievers = Arrays.asList(
+            ArgumentReflectionUtil::findByPublicApi, ArgumentReflectionUtil::findByReflectingJUnit512Context);
+
+    static List<NamedArgument> getNamedArgs(ExtensionContext context) {
+        List<NamedArgument> namedArgs = new ArrayList<>();
+        var exceptionStorage = new RuntimeException("Failed to retrieve parameter arguments");
+
+        if (context.getTestMethod().isPresent() && context.getTestMethod().get().getParameterCount() > 0) {
+            for (var retriever : argumentRetrievers) {
+                try {
+                    namedArgs = retriever.apply(context);
+                    if (!namedArgs.isEmpty()) {
+                        break;
+                    }
+                } catch (Exception | NoClassDefFoundError e) {
+                    exceptionStorage.addSuppressed(e);
+                }
+            }
+        }
+        if (exceptionStorage.getSuppressed().length > 0) {
+            log.atWarn().setCause(exceptionStorage).log(ERROR);
+        }
+        return namedArgs;
+    }
+
+    //ExperimentalAPI that only exists since JUnit 5.13
+    @SuppressWarnings("OptionalGetWithoutIsPresent") //Presence checked in calling method
+    private static List<NamedArgument> findByPublicApi(ExtensionContext context) {
+        var args = Arrays.asList(ParameterInfo.get(context).getArguments().toArray());
+        return ParameterNameUtil.mapArgumentsWithParameterNames(context.getTestMethod().get(), args);
+    }
+
     /**
      * This is a very ugly workaround to get the method arguments from the JUnit 5 context via reflection.
      */
-    static List<NamedArgument> getNamedArgs(ExtensionContext context) {
+    @SuppressWarnings("OptionalGetWithoutIsPresent") //Presence checked in calling method
+    private static List<NamedArgument> findByReflectingJUnit512Context(ExtensionContext context) {
         List<NamedArgument> namedArgs = new ArrayList<>();
-
-        if (context.getTestMethod().get().getParameterCount() > 0) {
-            try {
-                if (context.getClass().getCanonicalName().equals(METHOD_EXTENSION_CONTEXT)) {
-                    Field field = context.getClass().getSuperclass().getDeclaredField("testDescriptor");
-                    Object testDescriptor = ReflectionUtil.getFieldValueOrNull(field, context, ERROR);
-                    if (testDescriptor != null
-                            && testDescriptor.getClass().getCanonicalName().equals(TEST_TEMPLATE_INVOCATION_TEST_DESCRIPTOR)) {
-                        Object invocationContext = ReflectionUtil.getFieldValueOrNull("invocationContext", testDescriptor, ERROR);
-                        if (invocationContext != null
-                                && invocationContext.getClass().getCanonicalName().equals(PARAMETERIZED_TEST_INVOCATION_CONTEXT)) {
-                            Object arguments = ReflectionUtil.getFieldValueOrNull("arguments", invocationContext, ERROR);
-                            if (arguments instanceof Arguments) {
-                                List<Object> args = Arrays.asList(((Arguments) arguments).get());
-                                namedArgs = ParameterNameUtil.mapArgumentsWithParameterNames(context.getTestMethod().get(), args);
-                            } else {
-                                log.warn(ERROR + " The type of arguments in the invocation context has changed. Please write a bug report.");
-                            }
+        try {
+            if (context.getClass().getCanonicalName().equals(METHOD_EXTENSION_CONTEXT)) {
+                Field field = context.getClass().getSuperclass().getDeclaredField("testDescriptor");
+                Object testDescriptor = ReflectionUtil.getFieldValueOrNull(field, context, ERROR);
+                if (testDescriptor != null
+                        && testDescriptor.getClass().getCanonicalName().equals(TEST_TEMPLATE_INVOCATION_TEST_DESCRIPTOR)) {
+                    Object invocationContext = ReflectionUtil.getFieldValueOrNull("invocationContext", testDescriptor, ERROR);
+                    if (invocationContext != null
+                            && invocationContext.getClass().getCanonicalName().equals(PARAMETERIZED_TEST_INVOCATION_CONTEXT)) {
+                        Object arguments = ReflectionUtil.getFieldValueOrNull("arguments", invocationContext, ERROR);
+                        if (arguments instanceof Arguments) {
+                            List<Object> args = Arrays.asList(((Arguments) arguments).get());
+                            namedArgs = ParameterNameUtil.mapArgumentsWithParameterNames(context.getTestMethod().get(), args);
+                        } else {
+                            log.warn(ERROR + " The type of arguments in the invocation context has changed. Please write a bug report.");
                         }
                     }
                 }
-            } catch (Exception e) {
-                log.warn(ERROR, e);
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
         return namedArgs;
     }
 }
