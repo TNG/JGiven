7227604787f89cb5015d5f6a95dd0839f254485b
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-07-30 16:22:08 +0200

    fix(Release Plugin): create convention plugins for releases
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

---
+++ build-conventions/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+
+dependencies {
+    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
+}
