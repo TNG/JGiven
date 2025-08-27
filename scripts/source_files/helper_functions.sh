f332dcf7cd52e400033b47f0692a7f3e71ce6afa
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-08-27 15:28:56 +0200

    fix(Gradle upgrade): use -p parameter for example tasks
    -b parameter was dropped
    
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- scripts/source_files/helper_functions.sh
+++ scripts/source_files/helper_functions.sh
@@ -84,7 +84,7 @@ function runGradleTestOnGivenProject() {
     local givenProject="$1"
     local VERSION="$2"
 
-    ./gradlew -b $givenProject clean test -Pversion=$VERSION
+    ./gradlew -p $givenProject clean test -Pversion=$VERSION
 }
 
 function runScalaTest() {
