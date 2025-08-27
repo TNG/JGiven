372fe91f66180a56e46de814f9853c9e17bc832e
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-08-27 16:05:22 +0200

    fix(Gradle upgrade): fix -b option for android
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- scripts/source_files/helper_functions.sh
+++ scripts/source_files/helper_functions.sh
@@ -98,7 +98,7 @@ function runAndroidTestOnGivenProject() {
     local givenProject="$1"
     local VERSION="$2"
 
-    ./gradlew -b $givenProject clean test connectedAndroidTest -Pversion=$VERSION
+    ./gradlew -p $givenProject clean test connectedAndroidTest -Pversion=$VERSION
 }
 
 function runMavenTestOnGivenProject() {
