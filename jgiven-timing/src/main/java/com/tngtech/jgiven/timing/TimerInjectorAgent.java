0cf2e3b6aa60ff2293aebaa0d0064b1c4fe13162
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-10-10 16:07:46 +0200

    fix(): update java timing code
    now that I ran it again for once
    
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- jgiven-timing/src/main/java/com/tngtech/jgiven/timing/TimerInjectorAgent.java
+++ jgiven-timing/src/main/java/com/tngtech/jgiven/timing/TimerInjectorAgent.java
@@ -1,10 +1,11 @@
 package com.tngtech.jgiven.timing;
 
 import com.tngtech.jgiven.impl.ScenarioBase;
-import java.lang.instrument.Instrumentation;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.instrument.Instrumentation;
 
 /**
  * Class that acts as a Java agent and injects code before class for timing the test methods.
@@ -16,7 +17,7 @@ public class TimerInjectorAgent {
     public static void premain(String args, Instrumentation instrumentation) {
         new AgentBuilder.Default()
                 .type(ElementMatchers.isSubTypeOf(ScenarioBase.class))
-                .transform((builder, type, classLoader, module) ->
+                .transform((builder, type, classLoader, module, __) ->
                         builder.method(ElementMatchers.named("initialize")
                                 .or(ElementMatchers.named("finished")))
                                 .intercept(MethodDelegation.to(ManageTimerInterceptor.class))
