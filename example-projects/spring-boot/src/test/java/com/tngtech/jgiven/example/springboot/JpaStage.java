8841e8708cb74c141f8996818ad39cbf26b20bc3
l-1squared <30831153+l-1squared@users.noreply.github.com>
2026-06-11 07:28:23 +0200

    choe(Deps): Remove outdated Spring features
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- example-projects/spring-boot/src/test/java/com/tngtech/jgiven/example/springboot/JpaStage.java
+++ example-projects/spring-boot/src/test/java/com/tngtech/jgiven/example/springboot/JpaStage.java
@@ -1,8 +1,8 @@
 package com.tngtech.jgiven.example.springboot;
 
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JpaStage {
 
     @Autowired
-    TestEntityManager testEntityManager;
+    EntityManager testEntityManager;
 
 
     public JpaStage test_entity_manager_is_defined() {
