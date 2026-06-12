78578e2abb96a697fb0c54ef1fa3ef3430cf606a
Hendrik Vennekate <Hendrik.Vennekate@tngtech.com>
2026-06-12 17:29:29 +0200

    Use only utility method in `HierarchyCalculator`
    Signed-off-by: Hendrik Vennekate <Hendrik.Vennekate@tngtech.com>

--- jgiven-asciidoc-report/src/main/java/com/tngtech/jgiven/report/asciidoc/HierarchyCalculator.java
+++ jgiven-asciidoc-report/src/main/java/com/tngtech/jgiven/report/asciidoc/HierarchyCalculator.java
@@ -5,27 +5,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class HierarchyCalculator {
-    private final Map<String, List<String>> taggedScenarioFiles;
-    private final Map<String, Tag> allTags;
+import static java.util.stream.Collectors.groupingBy;
 
-    public HierarchyCalculator(final Map<String, Tag> allTags, final Map<String, List<String>> taggedScenarioFiles) {
-        this.taggedScenarioFiles = taggedScenarioFiles;
-        this.allTags = allTags;
+public class HierarchyCalculator {
+    private HierarchyCalculator() {
     }
 
     static Map<String, Map<String, List<String>>> computeGroupedTag(final Map<String, Tag> allTags,
             final Map<String, List<String>> taggedScenarioFiles) {
-        return new HierarchyCalculator(allTags, taggedScenarioFiles).computeGroupedTag();
-    }
-
-    Map<String, Map<String, List<String>>> computeGroupedTag() {
         return taggedScenarioFiles.entrySet().stream()
                 .filter(entry -> allTags.get(entry.getKey()).getShownInNavigation())
-                .collect(Collectors.groupingBy(this::fullType, Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+                .collect(groupingBy(entry -> fullType(entry, allTags), Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 
-    private String fullType(final Map.Entry<String, List<String>> entry) {
+    private static String fullType(final Map.Entry<String, List<String>> entry, Map<String, Tag> allTags) {
         final var tag = allTags.get(entry.getKey());
 
         return tag.getFullType();
